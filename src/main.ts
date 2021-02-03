a2035da7661e75636e5ac24240be5ddfe25c8974
jdeepee <joshuadparkin@gmail.com>
2021-02-03 14:56:23 +0000

    add infinite timer

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ const core = PerspectivismCore.create()
 const perspectivesController = core.perspectivesController();
 const languageController = core.languageController();
 
+// A micro service will exit when it has nothing left to do.  So to
+// avoid a premature exit, set an indefinite timer.  When we
+// exit() later, the timer will get invalidated.
+setInterval(()=>{}, 1000)
+
 LiquidCore.on('init', function() {
   core.initServices()
   core.startGraphQLServer()
@@ -12,3 +17,6 @@ LiquidCore.on('init', function() {
     core.initControllers()
   })
 })
+
+// Ok, we are all set up.  Let the host know we are ready to talk
+LiquidCore.emit( 'ready' )
