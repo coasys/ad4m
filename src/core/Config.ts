080ad06bec7c52347e1a756691e9856393deb808
jdeepee <joshuadparkin@gmail.com>
2021-04-12 21:06:25 +0100

    add temp path in config to allow for unpacking/packing of DNA's outside of language directories

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 export let rootConfigPath = path.join('', 'ad4m')
 export let dataPath = path.join(rootConfigPath, 'data')
 export let languagesPath = path.join(rootConfigPath, 'languages')
+export let tempLangPath = path.join(languagesPath, "temp")
 export let holochainPath = path.join(rootConfigPath, 'holochain')
 export let holochainConfigPath = path.join(holochainPath, 'config')
 export let holochainDataPath = path.join(holochainPath, 'data')
@@ -15,12 +16,13 @@ export function init(appDataPath, appResourcePath) {
     rootConfigPath = path.join(appDataPath, 'ad4m')
     dataPath = path.join(rootConfigPath, 'data')
     languagesPath = path.join(rootConfigPath, 'languages')
+    tempLangPath = path.join(languagesPath, "temp")
     holochainPath = path.join(rootConfigPath, 'holochain')
     holochainConfigPath = path.join(holochainPath, 'config')
     holochainDataPath = path.join(holochainPath, 'data')
 
     //Create paths if they do not exist
-    const dirs = [rootConfigPath, dataPath, languagesPath, holochainPath, holochainConfigPath, holochainDataPath]
+    const dirs = [rootConfigPath, dataPath, languagesPath, tempLangPath, holochainPath, holochainConfigPath, holochainDataPath]
     for(const d of dirs)
     if(!fs.existsSync(d)) {
         fs.mkdirSync(d)
