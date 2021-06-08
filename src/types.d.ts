de3f4fd587ae546bdf71f7d3328ace2c7f5c867d
Nicolas Luck <nicolas@lucksus.eu>
2021-06-08 20:42:35 +0200

    Fix hand-made types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,7 @@ import("./core/PerspectivismCore");  // Don't delete this line.
 import { PerspectivismCore } from "./core/PerspectivismCore";
 
 //Module types
-export interface Config {
+export interface OuterConfig {
   resourcePath: string
   appDataPath: string
   appDefaultLangPath: string
@@ -11,6 +11,21 @@ export interface Config {
   appBuiltInLangs: string[] | null,
   appLangAliases: object | null,
   mocks: boolean
+}
+
+export interface Config {
+  rootConfigPath: string
+  dataPath: string
+  languagesPath: string
+  tempLangPath: string
+  holochainPath: string
+  holochainDataPath: string
+  holochainConductorPath: string
+  resourcePath: string
+  builtInLangPath: string
+  builtInLangs: string
+  languageAliases: string
+  bootstrapFixtures: BootstrapFixtures|void
 }
 
 export function init(config: Config): Promise<PerspectivismCore>;
