60506cbf629220802c188e8c613bdad5fbef477d
Nicolas Luck <nicolas@lucksus.eu>
2021-07-07 22:16:04 +0200

    Expression getRaw() test

--- src/tests/expresssion.ts
+++ src/tests/expresssion.ts
@@ -5,11 +5,18 @@ export default function expressionTests(testContext: TestContext) {
         describe('Expressions', () => {
             it('can get() my agent expression', async () => {
                 const ad4mClient = testContext.ad4mClient
-
                 const me = await ad4mClient.agent.me()
                 
                 const agent = await ad4mClient.expression.get(me.did)
                 expect(JSON.parse(agent.data)).toEqual(me);
+            })
+
+            it('can getRaw() my agent expression', async () => {
+                const ad4mClient = testContext.ad4mClient
+                const me = await ad4mClient.agent.me()
+                
+                const agent = await ad4mClient.expression.getRaw(me.did)
+                expect(JSON.parse(agent).data).toEqual(me);
             })
         })
     }
