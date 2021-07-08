bd3d7da434dc2f91dd20060d0e7aaa65719c321c
jdeepee <joshuadparkin@gmail.com>
2021-07-08 15:05:27 +0100

    add test cases to check that holochain & ipfs processes are correctly killed on exit

--- src/tests/integration.test.ts
+++ src/tests/integration.test.ts
@@ -9,6 +9,7 @@ import main from "../main";
 import fs from 'fs-extra'
 import path from 'path'
 import ws from "ws"
+import os from "os";
 import { Ad4mClient } from "@perspect3vism/ad4m";
 
 // Patch Reflect to have missing getOwnPropertyDescriptor()
@@ -50,6 +51,25 @@ export class TestContext {
 }
 let testContext: TestContext = new TestContext()
 
+export async function isProcessRunning(processName: string): Promise<boolean> {
+    const cmd = (() => {
+      switch (process.platform) {
+        case 'win32': return `tasklist`
+        case 'darwin': return `ps -ax | grep ${processName}`
+        case 'linux': return `ps -A`
+        default: return false
+      }
+    })()
+  
+    return new Promise((resolve, reject) => {
+      require('child_process').exec(cmd, (err: Error, stdout: string, stderr: string) => {
+        if (err) reject(err)
+  
+        resolve(stdout.toLowerCase().indexOf(processName.toLowerCase()) > -1)
+      })
+    })
+  }
+
 describe("Integration tests", () => {
 
     beforeAll(async () => {
@@ -78,8 +98,17 @@ describe("Integration tests", () => {
     })
 
     afterAll(async () => {
-        await core.exit()
+        expect(await isProcessRunning("holochain")).toBe(true);
+        expect(await isProcessRunning("lair-keystore")).toBe(true);
+        expect(fs.existsSync(path.join(os.homedir(), ".jsipfs/repo.lock"))).toBe(true);
+
+        await core.exit();
         await new Promise((resolve)=>setTimeout(resolve, 1000))
+        
+        expect(await isProcessRunning("holochain")).toBe(false);
+        expect(await isProcessRunning("lair-keystore")).toBe(false);
+        expect(fs.existsSync(path.join(os.homedir(), ".jsipfs/repo.lock"))).toBe(false);
+
         //Delete all languages created during test
         fs.readdir(path.join(DATA_RESOURCE_PATH, "ad4m/languages"), (err, files) => {
             if (err) throw err;
