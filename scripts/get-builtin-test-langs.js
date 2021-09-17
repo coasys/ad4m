fdd432488f226fd1a207fd765639c9713c3a7bef
jdeepee <joshuadparkin@gmail.com>
2021-09-17 13:40:33 +0100

    use new centralized language language implementation

--- scripts/get-builtin-test-langs.js
+++ scripts/get-builtin-test-langs.js
@@ -12,8 +12,7 @@ const languages = {
   },
   languages: {
       targetDnaName: "languages",
-      dna: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.6/languages.dna",
-      bundle: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.6/bundle.js",
+      bundle: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.7/bundle.js",
   },
   "neighbourhood-store": {
     targetDnaName: "neighbourhood-store",
