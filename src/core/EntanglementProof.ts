db9ad3555b84f0885a3f9c8e2b19df6fad112451
jdeepee <joshuadparkin@gmail.com>
2021-10-05 20:52:49 +0100

    rename entanglement proof file variable

--- src/core/EntanglementProof.ts
+++ src/core/EntanglementProof.ts
@@ -5,11 +5,11 @@ import AgentService from "./agent/AgentService";
 import { EntanglementProof } from '@perspect3vism/ad4m';
 
 export default class EntanglementProofController {
-    #entanglementProofs: string
+    #entanglementProofsFile: string
     #agentService: AgentService
 
     constructor(rootConfigPath: string, agentService: AgentService) {
-        this.#entanglementProofs = path.join(rootConfigPath, "entanglementProofs.json")
+        this.#entanglementProofsFile = path.join(rootConfigPath, "entanglementProofs.json")
         this.#agentService = agentService
     }
 
@@ -23,30 +23,30 @@ export default class EntanglementProofController {
 
     addEntanglementProofs(proofs: EntanglementProof[]): void {
         let entanglementProofs: EntanglementProof[];
-        if (fs.existsSync(this.#entanglementProofs)) {
-            entanglementProofs = Array.from(JSON.parse(fs.readFileSync(this.#entanglementProofs).toString()));
+        if (fs.existsSync(this.#entanglementProofsFile)) {
+            entanglementProofs = Array.from(JSON.parse(fs.readFileSync(this.#entanglementProofsFile).toString()));
             entanglementProofs = entanglementProofs.concat(proofs);
             entanglementProofs = Array.from(new Set(entanglementProofs));
         } else {
             entanglementProofs = proofs 
         }
 
-        fs.writeFileSync(this.#entanglementProofs, JSON.stringify(entanglementProofs))
+        fs.writeFileSync(this.#entanglementProofsFile, JSON.stringify(entanglementProofs))
     }
 
     deleteEntanglementProofs(proofs: EntanglementProof[]): void {
-        if (fs.existsSync(this.#entanglementProofs)) {
-            let entanglementProofs = Array.from(JSON.parse(fs.readFileSync(this.#entanglementProofs).toString()));
+        if (fs.existsSync(this.#entanglementProofsFile)) {
+            let entanglementProofs = Array.from(JSON.parse(fs.readFileSync(this.#entanglementProofsFile).toString()));
             for (const agent of proofs) {
                 entanglementProofs.splice(entanglementProofs.findIndex((value) => value == agent), 1);
             }
-            fs.writeFileSync(this.#entanglementProofs, JSON.stringify(entanglementProofs))
+            fs.writeFileSync(this.#entanglementProofsFile, JSON.stringify(entanglementProofs))
         }
     }
 
     getEntanglementProofs(): EntanglementProof[] {
-        if (fs.existsSync(this.#entanglementProofs)) {
-            let entanglementProofs: EntanglementProof[] = Array.from(JSON.parse(fs.readFileSync(this.#entanglementProofs).toString()));
+        if (fs.existsSync(this.#entanglementProofsFile)) {
+            let entanglementProofs: EntanglementProof[] = Array.from(JSON.parse(fs.readFileSync(this.#entanglementProofsFile).toString()));
             return entanglementProofs
         } else {
             return [] 
