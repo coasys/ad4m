350a1c1cef0560ffc6dbf339ecf147501d8b604b
Nicolas Luck <nicolas@lucksus.eu>
2021-12-08 21:28:03 +0100

    Update Holonix and HC to 115

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
-
-{
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/2f8ca2fa76165e2978112cb693c572f1086c5541.tar.gz"; }
-}:
-
 let
-  holonix = import (holonixPath) { };
+  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/f3ecb117bdd876b8dcb33ad04984c5da5b2d358c.tar.gz";
+  holonix = import (holonixPath) {
+    holochainVersionId = "v0_0_115";
+  };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
-  inputsFrom = [ holonix.main ];
+  inputsFrom = [ holonix.shell ];
+  packages = [
+  ];
   buildInputs = with nixpkgs; [
     binaryen
     nodejs-16_x
