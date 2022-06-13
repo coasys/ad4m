89c85296750b5716e0660edb6564aedb619d66c0
Wesley Finck <wesleyfinck@gmail.com>
2022-06-13 09:58:37 -0700

    complete and passing integration test of social dna flows

--- src/tests/social-dna-flow.ts
+++ src/tests/social-dna-flow.ts
@@ -8,13 +8,11 @@ const path = require('path')
 export default function socialDNATests(testContext: TestContext) {
     return  () => {
         describe("There is a SDNA test exercising an example TODO SDNA", () => {
-            // read in prolog facts from todo.pl
-            // add each line as target in new link has_zome
-            it('can add social DNA to perspective', async () => {
+            it('can add social DNA to perspective and go through flow', async () => {
                 const prologRules = [
                     'register_sdna_flow("TODO", t).',
                     'flowable(_, t).',
-                    `start_action(t, '[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://ready"}').`,
+                    `start_action('[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://ready"}]', t).`,
                     'flow_state(ExprAddr, 0, t) :- triple(ExprAddr, "todo://state", "todo://ready").',
                     `action(0, "Start", 0.5, '[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://doing"}, {action: "removeLink", source: "this", predicate: "todo://state", target: "todo://ready"}]').`,
                     'flow_state(ExprAddr, 0.5, t) :- triple(ExprAddr, "todo://state", "todo://doing").',
@@ -28,7 +26,7 @@ export default function socialDNATests(testContext: TestContext) {
                 expect(perspective.name).toEqual("sdna-test");
 
                 for (const fact of prologRules) {
-                    perspective.add(new Link({
+                    await perspective.add(new Link({
                         source: 'self', 
                         predicate: 'ad4m://has_zome',
                         target: Literal.from(fact).toUrl(),
@@ -37,12 +35,63 @@ export default function socialDNATests(testContext: TestContext) {
 
                 let sDNAFacts = await ad4mClient!.perspective.queryLinks(perspective.uuid, new LinkQuery({source: "self", predicate: "ad4m://has_zome"}));
                 expect(sDNAFacts.length).toEqual(8);
-                // expect there to be 8 links off self
                 let flows = await perspective.sdnaFlows()
-                console.log('dna flows: ', flows)
                 expect(flows[0]).toBe('TODO')
-                perspective.add(new Link({source: 'self', target: 'test-lang://1234'}))
-                perspective.startFlow('TODO', 'test-lang://1234')
+
+                await perspective.add(new Link({source: 'self', target: 'test-lang://1234'}))
+                let availableFlows = await perspective.availableFlows('test-lang://1234')
+                expect(availableFlows.length).toEqual(1)
+                expect(availableFlows[0]).toEqual('TODO')
+                let startAction = await perspective.infer(`start_action(Action, F), register_sdna_flow("TODO", F)`)
+                await perspective.startFlow('TODO', 'test-lang://1234')
+
+                let flowLinks = await ad4mClient!.perspective.queryLinks(perspective.uuid, new LinkQuery({source: "test-lang://1234", predicate: "todo://state"}))
+                expect(flowLinks.length).toEqual(1)
+                expect(flowLinks[0].data.target).toEqual("todo://ready")
+
+                let todoState = await perspective.flowState('TODO', 'test-lang://1234')
+                expect(todoState).toEqual(0)
+
+                let expressionsInTodo = await perspective.expressionsInFlowState('TODO', 0)
+                expect(expressionsInTodo.length).toEqual(1)
+                expect(expressionsInTodo[0]).toEqual('test-lang://1234')
+
+
+                // continue flow
+                let flowActions = await perspective.flowActions('TODO', 'test-lang://1234')
+                expect(flowActions.length).toEqual(1)
+                expect(flowActions[0]).toEqual("Start")
+
+
+                await perspective.runFlowAction('TODO', 'test-lang://1234', "Start")
+                todoState = await perspective.flowState('TODO', 'test-lang://1234')
+                expect(todoState).toEqual(0.5)
+
+                flowLinks = await ad4mClient!.perspective.queryLinks(perspective.uuid, new LinkQuery({source: "test-lang://1234", predicate: "todo://state"}))
+                expect(flowLinks.length).toEqual(1)
+                expect(flowLinks[0].data.target).toEqual("todo://doing")
+
+                expressionsInTodo = await perspective.expressionsInFlowState('TODO', 0.5)
+                expect(expressionsInTodo.length).toEqual(1)
+                expect(expressionsInTodo[0]).toEqual('test-lang://1234')
+
+                // continue flow
+                flowActions = await perspective.flowActions('TODO', 'test-lang://1234')
+                expect(flowActions.length).toEqual(1)
+                expect(flowActions[0]).toEqual("Finish")
+
+
+                await perspective.runFlowAction('TODO', 'test-lang://1234', "Finish")
+                todoState = await perspective.flowState('TODO', 'test-lang://1234')
+                expect(todoState).toEqual(1)
+
+                flowLinks = await ad4mClient!.perspective.queryLinks(perspective.uuid, new LinkQuery({source: "test-lang://1234", predicate: "todo://state"}))
+                expect(flowLinks.length).toEqual(1)
+                expect(flowLinks[0].data.target).toEqual("todo://done")
+                expressionsInTodo = await perspective.expressionsInFlowState('TODO', 1)
+                expect(expressionsInTodo.length).toEqual(1)
+                expect(expressionsInTodo[0]).toEqual('test-lang://1234')
+
             }) 
         })
     }
