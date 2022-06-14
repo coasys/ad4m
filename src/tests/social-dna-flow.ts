de4dc1a3b9ab4ea38a8f4f39565b475b576b895e
Nicolas Luck <nicolas@lucksus.eu>
2022-06-14 17:55:07 +0200

    Add comments to SDNA and add code as one expression

--- src/tests/social-dna-flow.ts
+++ src/tests/social-dna-flow.ts
@@ -9,32 +9,40 @@ export default function socialDNATests(testContext: TestContext) {
     return  () => {
         describe("There is a SDNA test exercising an example TODO SDNA", () => {
             it('can add social DNA to perspective and go through flow', async () => {
-                const prologRules = [
+                const sdna = [
+                    // The name of our SDNA flow: "TODO"
                     'register_sdna_flow("TODO", t).',
+
+                    // What expressions can be used to start this flow? -> all
                     'flowable(_, t).',
-                    `start_action('[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://ready"}]', t).`,
+
+                    // This Flow has 3 states (0=ready, 0.5=doing, 1=done), 
+                    // which are represented by links with predicate 'todo://state'
                     'flow_state(ExprAddr, 0, t) :- triple(ExprAddr, "todo://state", "todo://ready").',
-                    `action(0, "Start", 0.5, '[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://doing"}, {action: "removeLink", source: "this", predicate: "todo://state", target: "todo://ready"}]').`,
                     'flow_state(ExprAddr, 0.5, t) :- triple(ExprAddr, "todo://state", "todo://doing").',
+                    'flow_state(ExprAddr, 1, t) :- triple(ExprAddr, "todo://state", "todo://done").',
+
+                    // Initial action renders any expression into a todo item by adding a state link to 'ready'
+                    `start_action('[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://ready"}]', t).`,
+                    // A ready todo can be 'started' = commencing work on it. Removes 'ready' link and replaces it by 'doing' link
+                    `action(0, "Start", 0.5, '[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://doing"}, {action: "removeLink", source: "this", predicate: "todo://state", target: "todo://ready"}]').`,
+                    // A todo in doing can be 'finished'
                     `action(0.5, "Finish", 1, '[{action: "addLink", source: "this", predicate: "todo://state", target: "todo://done"}, {action: "removeLink", source: "this", predicate: "todo://state", target: "todo://doing"}]').`,
-                    'flow_state(ExprAddr, 1, t) :- triple(ExprAddr, "todo://state", "todo://done").'
                 ]
 
                 const ad4mClient = testContext.ad4mClient!
 
                 const perspective = await ad4mClient.perspective.add("sdna-test");
                 expect(perspective.name).toEqual("sdna-test");
-
-                for (const fact of prologRules) {
-                    await perspective.add(new Link({
-                        source: 'self', 
-                        predicate: 'ad4m://has_zome',
-                        target: Literal.from(fact).toUrl(),
-                    }))
-                }
+            
+                await perspective.add(new Link({
+                    source: 'self', 
+                    predicate: 'ad4m://has_zome',
+                    target: Literal.from(sdna.join('\n')).toUrl(),
+                }))
 
                 let sDNAFacts = await ad4mClient!.perspective.queryLinks(perspective.uuid, new LinkQuery({source: "self", predicate: "ad4m://has_zome"}));
-                expect(sDNAFacts.length).toEqual(8);
+                expect(sDNAFacts.length).toEqual(1);
                 let flows = await perspective.sdnaFlows()
                 expect(flows[0]).toBe('TODO')
 
