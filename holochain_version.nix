0df4858aa02c9d51c87fc57206901e9676529307
Joshua Parkin <joshuadparkin@gmail.com>
2022-07-01 10:50:17 +0100

    bump holochain & lair

--- holochain_version.nix
+++ holochain_version.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --git-src=revision:holochain-0.0.127 --lair-version-req=~0.1 --output-file=holochain_version.nix
+# update-holochain-versions --git-src=revision:holochain-0.0.146 --lair-version-req=~0.2 --output-file=holochain_version.nix
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "holochain-0.0.127";
-    sha256 = "sha256-NfrasLnSblIh4Rhfhh95qeEtG+9Tihfcn+CIgRpnwus=";
+    rev = "holochain-0.0.146";
+    sha256 = "sha256-RuEzu4U9dOei8L5MOoLvIM50czh8mzg+tCo8QCg815I=";
     cargoLock = {
         outputHashes = {
         };
@@ -21,8 +21,8 @@
 
     lair = {
         url = "https://github.com/holochain/lair";
-        rev = "v0.1.0";
-        sha256 = "sha256-EWtc9+KcN/RHOg5N4wCLBXRppXheda5uubwrQVojc0s=";
+        rev = "lair_keystore_api-v0.2.0";
+        sha256 = "sha256-n7nZyZR0Q68Uff7bTSVFtSDLi21CNcyKibOBx55Gasg=";
 
         binsFilter = [
             "lair-keystore"
