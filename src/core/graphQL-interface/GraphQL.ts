6c4fcc76719483bd83682432d12faa30702fbde9
fayeed <fayeed@live.com>
2022-07-05 20:08:08 +0530

    Added agent status change event on agent initiazation

--- src/core/graphQL-interface/GraphQL.ts
+++ src/core/graphQL-interface/GraphQL.ts
@@ -289,10 +289,14 @@ function createResolvers(core: PerspectivismCore, config: any) {
                     await core.initLanguages()
                     await core.initializeAgentsDirectMessageLanguage()
                 }
+
+                const agent = core.agentService.dump();
+
+                pubsub.publish(PubSub.AGENT_STATUS_CHANGED, agent)
                 
                 console.log("\x1b[32m", "AD4M init complete", "\x1b[0m");
                 
-                return core.agentService.dump()
+                return agent;
             },
             //@ts-ignore
             agentImport: async (parent, args, context, info) => {
