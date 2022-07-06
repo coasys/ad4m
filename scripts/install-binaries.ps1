cde39048187f78c65665117146af8c1d7cda9f24
Fayeed Pawaskar <fayeed@live.com>
2022-07-06 11:22:23 +0530

    Added force to install binaries

--- scripts/install-binaries.ps1
+++ scripts/install-binaries.ps1
@@ -6,7 +6,7 @@ if (Test-Path $HoloPath) {
   $Version = Invoke-Expression "$HoloPath --version"
 
   if($Version -notlike '*0.0.146*') {
-    cargo install --locked holochain --git https://github.com/holochain/holochain.git --tag holochain-0.0.146
+    cargo install --locked holochain --git https://github.com/holochain/holochain.git --tag holochain-0.0.146 --force
   }
 }
 
@@ -16,7 +16,7 @@ if (Test-Path $HcPath) {
   $Version = Invoke-Expression "$HcPath --version"
 
   if($Version -notlike '*0.0.44*') {
-    cargo install holochain_cli --version 0.0.44
+    cargo install holochain_cli --version 0.0.44 --force
   }
 }
 
@@ -26,6 +26,6 @@ if (Test-Path $LKPath) {
   $Version = Invoke-Expression "$LKPath --version"
 
   if($Version -notlike '*0.2.0*') {
-    cargo install lair_keystore --version 0.2.0
+    cargo install lair_keystore --version 0.2.0 --force
   }
 }
