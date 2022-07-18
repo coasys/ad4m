c4b6fe842a3456638d38a40a72ed7c23c520455d
Nicolas Luck <nicolas@lucksus.eu>
2022-07-19 00:20:54 +0200

    Use mutex to ensure Prolog queries not run while engine gets respawned

--- src/core/Perspective.ts
+++ src/core/Perspective.ts
@@ -6,6 +6,7 @@ import type LanguageController from "./LanguageController";
 import * as PubSub from './graphQL-interface/PubSub'
 import type PerspectiveContext from "./PerspectiveContext"
 import PrologInstance from "./PrologInstance";
+import { Mutex } from 'async-mutex'
 
 export default class Perspective {
     name?: string;
@@ -22,6 +23,7 @@ export default class Perspective {
 
     #prologEngine: PrologInstance|null
     #prologNeedsRebuild: boolean
+    #prologMutex: Mutex
 
     constructor(id: PerspectiveHandle, context: PerspectiveContext, neighbourhood?: Neighbourhood) {
         this.updateFromId(id)
@@ -56,6 +58,8 @@ export default class Perspective {
         this.callLinksAdapter("pull").then((remoteLinks) => {
             this.populateLocalLinks(remoteLinks.additions, remoteLinks.removals);
         });
+
+        this.#prologMutex = new Mutex()
     }
 
     plain(): PerspectiveHandle {
@@ -493,11 +497,12 @@ export default class Perspective {
 
     async prologQuery(query: string): Promise<any> {
         await this.callLinksAdapter("pull");
-        if(!this.#prologEngine || this.#prologNeedsRebuild) {
-            this.#prologNeedsRebuild = false
-            await this.spawnPrologEngine()
-        }
-            
+        await this.#prologMutex.runExclusive(async () => {
+            if(!this.#prologEngine || this.#prologNeedsRebuild) {
+                this.#prologNeedsRebuild = false
+                await this.spawnPrologEngine()
+            }
+        })
         
         return await this.#prologEngine!.query(query)
     }
