082b5c459415c68aed386706b26147e477a8fa69
kaichao <kaichaosuna@gmail.com>
2022-07-26 10:04:40 +0800

    Update src/core/agent/Auth.ts
    Co-authored-by: Nicolas Luck <nicolas@lucksus.eu>

--- src/core/agent/Auth.ts
+++ src/core/agent/Auth.ts
@@ -359,7 +359,7 @@ export const checkCapability = (capabilities: Capabilities, expected: Capability
     }
 }
 
-export const DefaultTokenValidPeriod = 30 * 24 * 60 * 60; // 30 days in seconds
+export const DefaultTokenValidPeriod = 180 * 24 * 60 * 60; // 180 days in seconds
 
 export interface AuthInfoExtended {
     requestId: string,
