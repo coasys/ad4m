0192b3398888436f45f64e7cc94ebba0b47d7275
jdeepee <joshuadparkin@gmail.com>
2022-09-12 14:16:29 +0100

    bump holochain 161

--- holochain_version.nix
+++ holochain_version.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --git-src=revision:holochain-0.0.151 --lair-version-req=~0.2 --output-file=holochain_version.nix
+# update-holochain-versions --git-src=revision:holochain-0.0.161 --lair-version-req=~0.2 --output-file=holochain_version.nix
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "holochain-0.0.151";
-    sha256 = "sha256-llnVcv9zVnFI1CgzhYEoGQKWLG2uGHJIAJtMwfHPN4Y=";
+    rev = "holochain-0.0.161";
+    sha256 = "sha256-xp1DTVrhGEc1CZr6LvBZEYhoOUbUPpg3/mWOj4DDXjI=";
     cargoLock = {
         outputHashes = {
         };
