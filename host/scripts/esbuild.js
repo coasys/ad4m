f0c07eb90515685aba65d5a0731de67b447222fc
Fayeed <fayeed@live.com>
2022-11-02 18:29:29 +0530

    js bundles runs correctly

--- host/scripts/esbuild.js
+++ host/scripts/esbuild.js
@@ -1,40 +1,13 @@
-import fs from "fs";
-import path from "path";
 import { build } from 'esbuild' 
-
-const nodeModules = new RegExp(/^(?:.*[\\\/])?node_modules(?:[\\\/].*)?$/);
-
-const dirnamePlugin = {
-  name: "dirname",
-
-  setup(build) {
-    build.onLoad({ filter: /.*/ }, ({ path: filePath }) => {
-      if (!filePath.match(nodeModules)) {
-        let contents = fs.readFileSync(filePath, "utf8");
-        const loader = path.extname(filePath).substring(1);
-        const dirname = path.dirname(filePath);
-        console.log('wow', dirname)
-        contents = contents
-          .replace("__dirname", `"${dirname}"`)
-          .replace("__filename", `"${filePath}"`);
-        return {
-          contents,
-          loader,
-        };
-      }
-    });
-  },
-};
-
 build({
     entryPoints: ["./src/cli.ts"],
     platform: 'node',
-    plugins: [dirnamePlugin],
     bundle: true,
     format: "cjs",
     minify: false,
     sourcemap: false,
     outfile: "build/cli.cjs",
+    external: ['classic-level', 'default-gateway'],
     watch: process.env.NODE_ENV === "dev" ? true : false,
   })
   .catch((er) => {
