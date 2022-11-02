77dcbefe1cf5980d4b4c44d2fe9fe59acbfe3666
Fayeed <fayeed@live.com>
2022-11-02 10:58:42 +0530

    Updated script

--- executor/scripts/prepareTestDirectory.ps1
+++ executor/scripts/prepareTestDirectory.ps1
@@ -7,14 +7,11 @@ mkdir languages
 mkdir languages\test-language
 cp -r ..\tests\test-language\build languages\test-language\build
 
-wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/hc-windows-0.0.56.exe
-Copy-Item hc-windows-0.0.56.exe -Destination hc.exe
+wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/hc-windows-0.0.56.exe -O ./hc.exe
 
-wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/holochain-windows-0.0.161.exe
-Copy-Item holochain-windows-0.0.161.exe -Destination temp/binary/holochain.exe
+wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/holochain-windows-0.0.161.exe -O ./holochain.exe
 
-wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/lair-keystore-windows-0.2.0.exe
-Copy-Item lair-keystore-windows-0.2.0.exe -Destination temp/binary/lair-keystore.exe
+wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/lair-keystore-windows-0.2.0.exe -O ./lair-keystore.exe
 
 $SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
 Copy-Item $SwiplPath -Destination swipl.exe
