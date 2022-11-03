60b6eb0c95035dbef934598ad9f9d13c4f568475
Fayeed <fayeed@live.com>
2022-11-03 09:30:57 +0530

    Fixed windows packaging script for ad4m-host

--- host/scripts/download-binaries-windows.ps1
+++ host/scripts/download-binaries-windows.ps1
@@ -2,11 +2,8 @@ choco install wget --no-progress
         
 mkdir temp/binary
 
-wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/hc-windows-0.0.56.exe
-Copy-Item hc-windows-0.0.56.exe -Destination temp/binary/hc.exe
+wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/hc-windows-0.0.56.exe -O ./temp/binary/hc.exe
 
-wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/holochain-windows-0.0.161.exe
-Copy-Item holochain-windows-0.0.161.exe -Destination temp/binary/holochain.exe
+wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/holochain-windows-0.0.161.exe -O ./temp/binary/holochain.exe
 
-wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/lair-keystore-windows-0.2.0.exe
-Copy-Item lair-keystore-windows-0.2.0.exe -Destination temp/binary/lair-keystore.exe
+wget https://github.com/perspect3vism/ad4m-host/releases/download/binary-deps-0.0.161/lair-keystore-windows-0.2.0.exe -O ./temp/binary/lair-keystore.exe
