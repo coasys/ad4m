fa52397c5b9ea7cce701fa675b5c59bc2c1e871c
Nicolas Luck <nicolas@lucksus.eu>
2022-11-04 13:24:28 +0100

    Initial cli Rust project with clap and anyhow

---
+++ cli/src/main.rs
@@ -0,0 +1,35 @@
+extern crate clap;
+extern crate anyhow;
+
+use clap::Parser;
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
+}
+/// AD4M command line client
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Name of the person to greet
+   #[arg(short, long)]
+   name: String,
+
+   /// Number of times to greet
+   #[arg(short, long, default_value_t = 1)]
+   count: u8,
+}
+
+/*
+fn main() {
+   let args = Args::parse();
+
+   for _ in 0..args.count {
+       println!("Hello {}!", args.name)
+   }
+}
+ */
