cf4e4d45f3f16f65e7bac4257db8dab0a85529db
Kaicho Sun <kaichaosuna@gmail.com>
2022-11-21 22:26:09 +0800

    integrate login process.

--- ui/src-tauri/src/commands/proxy.rs
+++ ui/src-tauri/src/commands/proxy.rs
@@ -1,4 +1,4 @@
-use localtunnel::open_tunnel;
+use localtunnel_client::open_tunnel;
 use tauri::State;
 use tokio::sync::broadcast;
 
@@ -8,13 +8,38 @@ use crate::{AppState, ProxyState, ProxyService};
 pub async fn setup_proxy(subdomain: String, app_state: State<'_, AppState>, proxy: State<'_, ProxyState>) -> Result<String, String> {
     let local_port = app_state.graphql_port;
     let (notify_shutdown, _) = broadcast::channel(1);
+    let subdomain = subdomain.replace(":", "-").to_lowercase();
+
+    let rand = reqwest::get(format!("https://proxy-worker.ad4m.dev/login?did={}", subdomain))
+        .await
+        .map_err(|err| format!("Error happend when send login request: {:?}", err))?
+        .text()
+        .await
+        .map_err(|err| format!("Error happend when retrieving the content: {:?}", err))?;
+
+    // let {public_key, signature} = ad4m.agent.sign_message(rand)
+
+    let public_key = "demo-key";
+    let signature = "demo-signature";
+    let credential = reqwest::get(
+            format!(
+                "https://proxy-worker.ad4m.dev/login/verify?did={}&signature={}&publicKey={}",
+                subdomain, signature, public_key
+            ))
+        .await
+        .map_err(|err| format!("Error happend when send login verify request: {:?}", err))?
+        .text()
+        .await
+        .map_err(|err| format!("Error happend when retrieving the login verify content: {:?}", err))?;
 
     let endpoint = open_tunnel(
         Some("http://proxy.ad4m.dev"),
-        Some(&subdomain.replace(":", "-").to_lowercase()),
+        Some(&subdomain),
         None,
         local_port,
         notify_shutdown.clone(),
+        5,
+        Some(credential),
     )
     .await
     .map_err(|err| format!("Error happend when setup proxy: {:?}", err))?;
