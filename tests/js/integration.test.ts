cb5cb9b6c0853c47c6ec56033eee67e8d95e86c0
Nicolas Luck <nicolas@lucksus.eu>
2022-11-30 22:47:40 +0100

    WIP: JS integration tests

---
+++ tests/js/integration.test.ts
@@ -0,0 +1,91 @@
+import { expect } from "chai";
+import { exec, execSync, ChildProcess } from 'node:child_process';
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions/index.js";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core/index.js";
+import { HttpLink } from "@apollo/client/link/http/index.js";
+import Websocket from "ws";
+import { createClient } from "graphql-ws";
+import { Ad4mClient } from "@perspect3vism/ad4m";
+import { rmSync } from "node:fs";
+import fetch from 'node-fetch';
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function apolloClient(port: number, token?: string): ApolloClient<any> {
+    const wsLink = new GraphQLWsLink(createClient({
+        url: `ws://localhost:${port}/graphql`,
+        webSocketImpl: Websocket,
+        /*connectionParams: () => {
+            return {
+                headers: {
+                    authorization: token
+                }
+            }
+        },*/
+    }));
+
+    const link = new HttpLink({
+        uri: "http://localhost:4000/graphql",
+        //@ts-ignore
+        fetch
+      });
+  
+    return new ApolloClient({
+        link: wsLink,
+        cache: new InMemoryCache({ resultCaching: false, addTypename: false }),
+        defaultOptions: {
+            watchQuery: {
+                fetchPolicy: "no-cache",
+            },
+            query: {
+                fetchPolicy: "no-cache",
+            }
+        },
+    });
+  }
+
+describe("Integration", () => {
+    let ad4m: Ad4mClient | null = null
+    let executorProcess: ChildProcess | null = null
+
+    before(async () => {
+        rmSync("../ad4mJS", { recursive: true, force: true })
+        console.log("Initialzing executor data directory")
+        //@ts-ignore
+        execSync('../../host/dist/ad4m-macos-x64 init --dataPath ../ad4mJS', {})
+        
+        console.log("Starting executor")
+        //@ts-ignore
+        executorProcess = exec('../../host/dist/ad4m-macos-x64 serve --dataPath ../ad4mJS', {})
+        executorProcess!.stdout!.on('data', (data) => {
+            console.log(`${data}`);
+        });
+        //Log holochain process stderr to out
+        executorProcess!.stderr!.on('data', (data) => {
+            console.log(`${data}`);
+        });
+    
+        console.log("Waiting for executor to settle...")
+        await sleep(10000)
+        console.log("Creating ad4m client")
+        ad4m = new Ad4mClient(apolloClient(4000))
+        console.log("Generating agent")
+        await ad4m.agent.generate("secret")
+        console.log("Done")
+    })
+
+    after(() => {
+        //if (executorProcess) {
+        //    executorProcess.kill()
+        //}
+    })
+
+    it("should get agent status", async () => {
+        let result = await ad4m!.agent.status()
+        expect(result).to.not.be.null
+        expect(result!.isInitialized).to.not.be.true
+    })
+})
