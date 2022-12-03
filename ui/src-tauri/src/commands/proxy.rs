04b6e3ff5a4a6bfb41563109996681ba9ce6be74
Kaicho Sun <kaichaosuna@gmail.com>
2022-12-03 15:21:53 +0800

    short subdomain

--- ui/src-tauri/src/commands/proxy.rs
+++ ui/src-tauri/src/commands/proxy.rs
@@ -10,22 +10,35 @@ const AD4M_SERVER: &str = "http://127.0.0.1";
 
 #[tauri::command]
 pub async fn setup_proxy(subdomain: String, app_state: State<'_, AppState>, proxy: State<'_, ProxyState>) -> Result<String, String> {
+    log::info!("Setup proxy: {}", subdomain);
+
     let graphql_port = app_state.graphql_port;
     let req_credential = &app_state.req_credential;
     let (notify_shutdown, _) = broadcast::channel(1);
-    let subdomain = subdomain.replace(":", "-").to_lowercase();
+    let subdomain = subdomain.replace("did:key:", "").to_lowercase();
+    let max_len = std::cmp::min(32, subdomain.len());
+    let subdomain = &subdomain[0..max_len];
 
     let rand = reqwest::get(format!("{}/login?did={}", PROXY_SERVER, subdomain))
         .await
-        .map_err(|err| format!("Error happend when send login request: {:?}", err))?
+        .map_err(|err| {
+            log::error!("Error happend when send login request: {:?}", err);
+            format!("Set proxy error: {:?}", err)
+        })?
         .text()
         .await
-        .map_err(|err| format!("Error happend when retrieving the content: {:?}", err))?;
+        .map_err(|err| {
+            log::error!("Error happend when retrieving the content: {:?}", err);
+            format!("Set proxy error:  {:?}", err)
+        })?;
 
     let ad4m_client = Ad4mClient::new(format!("{}:{}/graphql", AD4M_SERVER, graphql_port), req_credential.to_string());
     let signed_message = ad4m_client.agent.sign_message(rand)
         .await
-        .map_err(|err| format!("Error happend when agent sign message: {:?}", err))?;
+        .map_err(|err| {
+            log::error!("Error happend when agent sign message: {:?}", err);
+            format!("Set proxy error:  {:?}", err)
+        })?;
 
     let credential = reqwest::get(
             format!(
@@ -33,10 +46,16 @@ pub async fn setup_proxy(subdomain: String, app_state: State<'_, AppState>, prox
                 PROXY_SERVER, subdomain, signed_message.signature, signed_message.public_key
             ))
         .await
-        .map_err(|err| format!("Error happend when send login verify request: {:?}", err))?
+        .map_err(|err| {
+            log::error!("Error happend when send login verify request: {:?}", err);
+            format!("Set proxy error:  {:?}", err)
+        })?
         .text()
         .await
-        .map_err(|err| format!("Error happend when retrieving the login verify content: {:?}", err))?;
+        .map_err(|err| {
+            log::error!("Error happend when retrieving the login verify content: {:?}", err);
+            format!("Set proxy error:  {:?}", err)
+        })?;
 
     let endpoint = open_tunnel(
         Some(PROXY_SERVER),
@@ -48,7 +67,11 @@ pub async fn setup_proxy(subdomain: String, app_state: State<'_, AppState>, prox
         Some(credential),
     )
     .await
-    .map_err(|err| format!("Error happend when setup proxy: {:?}", err))?;
+    .map_err(|err| {
+        log::error!("Error happend when open proxy: {:?}", err);
+        format!("Error happend when setup proxy: {:?}", err)
+    })?;
+    log::info!("Proxy endpoint: {}", endpoint);
 
     *proxy.0.lock().unwrap() = Some(ProxyService{
         endpoint: endpoint.clone(),
