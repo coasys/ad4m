b265abe30021ff9408804fc03d3f647e93cbe655
Leif Riksheim <riksheim.leif@gmail.com>
2022-12-19 17:54:44 +0100

    chore: modal title

--- ui/src/components/Auth.tsx
+++ ui/src/components/Auth.tsx
@@ -1,23 +1,20 @@
-import { useContext, useEffect, useState } from 'react';
-import { Ad4minContext } from '../context/Ad4minContext';
-import { showNotification } from '@mantine/notifications';
+import { useContext, useEffect, useState } from "react";
+import { Ad4minContext } from "../context/Ad4minContext";
+import { showNotification } from "@mantine/notifications";
 
 interface Capability {
-  with: Resource,
-  can: string[],
+  with: Resource;
+  can: string[];
 }
 
 interface Resource {
-  domain: string,
-  pointers: string[],
+  domain: string;
+  pointers: string[];
 }
 
 const Auth = () => {
   const {
-    state: {
-      client,
-      auth,
-    }
+    state: { client, auth },
   } = useContext(Ad4minContext);
 
   const [requestModalOpened, setRequestModalOpened] = useState(true);
@@ -26,7 +23,7 @@ const Auth = () => {
 
   useEffect(() => {
     setRequestModalOpened(true);
-  }, [auth])
+  }, [auth]);
 
   const authInfo = JSON.parse(auth).auth;
 
@@ -39,38 +36,33 @@ const Auth = () => {
 
     setSecretCode(result);
     setSecretCodeModalOpened(true);
-  }
+  };
 
   const closeRequestModal = () => {
-    setRequestModalOpened(false)
-  }
+    setRequestModalOpened(false);
+  };
 
   const closeSecretCodeModal = () => {
-    setSecretCodeModalOpened(false)
-  }
+    setSecretCodeModalOpened(false);
+  };
 
   const showCapabilities = (capabilities: Capability[]) => {
     return (
-      <j-flex
-        direction="column"
-        gap="200"
-        a="flex-start"
-      >
-        {
-          capabilities.map(cap => <j-text>{`${cap.can} => ${cap.with.domain}.${cap.with.pointers}`}</j-text>)
-        }
+      <j-flex direction="column" gap="200" a="flex-start">
+        {capabilities.map((cap) => (
+          <j-text>{`${cap.can} => ${cap.with.domain}.${cap.with.pointers}`}</j-text>
+        ))}
       </j-flex>
-
-    )
-  }
+    );
+  };
 
   const copyCode = () => {
     navigator.clipboard.writeText(secretCode);
     showNotification({
-      message: 'Secret code copied to clipboard',
-      autoClose: 1000
+      message: "Secret code copied to clipboard",
+      autoClose: 1000,
     });
-  }
+  };
 
   return (
     <div>
@@ -79,12 +71,13 @@ const Auth = () => {
         open={requestModalOpened}
         onToggle={(e: any) => setRequestModalOpened(e.target.open)}
       >
-        <j-box p="400">
+        <j-box px="400" py="600">
           <j-flex gap="200" direction="column">
-            <j-text nomargin variant="heading-sm">
-            Request Capabilities
-            </j-text>
-            <j-box p="200"></j-box>
+            <j-box pb="500">
+              <j-text nomargin size="600" color="black" weight="600">
+                Authorize Capabilities
+              </j-text>
+            </j-box>
             <j-input
               label="App Name"
               size="lg"
@@ -106,15 +99,11 @@ const Auth = () => {
             <j-text>Request for these capabilities,</j-text>
             {showCapabilities(authInfo.capabilities)}
             <j-box p="200"></j-box>
-              <j-flex>
-                <j-button onClick={closeRequestModal}>
-                Close
-                </j-button>
-                <j-box p="200"></j-box>
-                <j-button onClick={permitCapability} >
-                Confirm
-                </j-button>
-              </j-flex>
+            <j-flex>
+              <j-button onClick={closeRequestModal}>Close</j-button>
+              <j-box p="200"></j-box>
+              <j-button onClick={permitCapability}>Confirm</j-button>
+            </j-flex>
           </j-flex>
         </j-box>
       </j-modal>
@@ -128,23 +117,21 @@ const Auth = () => {
           <j-flex gap="200" direction="column">
             <j-flex a="center" gap="200">
               <j-text nomargin variant="heading-sm">
-              {secretCode}
+                {secretCode}
                 <j-Button variant="subtle" onClick={copyCode}>
                   <j-icon size="sm" name="clipboard"></j-icon>
                 </j-Button>
               </j-text>
             </j-flex>
             <j-box p="200"></j-box>
-              <j-flex>
-                <j-button onClick={closeSecretCodeModal}>
-                Close
-                </j-button>
-              </j-flex>
+            <j-flex>
+              <j-button onClick={closeSecretCodeModal}>Close</j-button>
+            </j-flex>
           </j-flex>
         </j-box>
       </j-modal>
     </div>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
