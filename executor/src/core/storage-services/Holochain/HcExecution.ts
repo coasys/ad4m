6abf6c5aed97744cba89e2f5087093c7a356a30f
Joshua Parkin <joshuadparkin@gmail.com>
2022-12-21 18:11:55 -0800

    new holochain conductor tuning parameters

--- executor/src/core/storage-services/Holochain/HcExecution.ts
+++ executor/src/core/storage-services/Holochain/HcExecution.ts
@@ -62,36 +62,39 @@ network:
         proxy_url: "${conductorConfig.proxyUrl}"
   tuning_params:
     gossip_strategy: sharded-gossip
-    gossip_loop_iteration_delay_ms: "1000"
-    gossip_outbound_target_mbps: "10"
-    gossip_inbound_target_mbps: "10"
-    gossip_historic_outbound_target_mbps: "10"
-    gossip_historic_inbound_target_mbps: "10"
-    gossip_burst_ratio: "100"
-    gossip_peer_on_success_next_gossip_delay_ms: "60000"
-    gossip_peer_on_error_next_gossip_delay_ms: "300000"
-    gossip_agent_info_update_interval_ms: "300000"
-    gossip_local_sync_delay_ms: "60000"
-    gossip_redundancy_target: "100"
-    gossip_max_batch_size: "16000000"
-    gossip_dynamic_arcs: "true"
-    gossip_single_storage_arc_per_space: "false"
-    default_rpc_single_timeout_ms: "30000"
-    default_rpc_multi_remote_agent_count: "3"
-    default_rpc_multi_remote_request_grace_ms: "3000"
-    agent_info_expires_after_ms: "1200000"
-    tls_in_mem_session_storage: "512"
-    proxy_keepalive_ms: "120000"
-    proxy_to_expire_ms: "300000"
-    concurrent_limit_per_thread: "4096"
-    tx2_quic_max_idle_timeout_ms: "30000"
-    tx2_pool_max_connection_count: "4096"
-    tx2_channel_count_per_connection: "2"
-    tx2_implicit_timeout_ms: "30000"
-    tx2_initial_connect_retry_delay_ms: "200"
+    gossip_loop_iteration_delay_ms: '1000'
+    gossip_outbound_target_mbps: '100'
+    gossip_inbound_target_mbps: '100'
+    gossip_historic_outbound_target_mbps: '100'
+    gossip_historic_inbound_target_mbps: '100'
+    gossip_burst_ratio: '100'
+    gossip_peer_on_success_next_gossip_delay_ms: '60000'
+    gossip_peer_on_error_next_gossip_delay_ms: '300000'
+    gossip_agent_info_update_interval_ms: '300000'
+    gossip_local_sync_delay_ms: '60000'
+    gossip_redundancy_target: '100'
+    gossip_max_batch_size: '1000000'
+    gossip_dynamic_arcs: 'true'
+    gossip_single_storage_arc_per_space: 'false'
+    default_rpc_single_timeout_ms: '60000'
+    default_rpc_multi_remote_agent_count: '3'
+    default_rpc_multi_remote_request_grace_ms: '3000'
+    agent_info_expires_after_ms: '1200000'
+    tls_in_mem_session_storage: '512'
+    proxy_keepalive_ms: '120000'
+    proxy_to_expire_ms: '300000'
+    concurrent_limit_per_thread: '4096'
+    tx2_quic_max_idle_timeout_ms: '60000'
+    tx2_pool_max_connection_count: '4096'
+    tx2_channel_count_per_connection: '2'
+    tx2_implicit_timeout_ms: '60000'
+    tx2_initial_connect_retry_delay_ms: '200'
     danger_tls_keylog: no_keylog
-    danger_gossip_recent_threshold_secs: "900"
-    disable_publish: "false"
+    danger_gossip_recent_threshold_secs: '900'
+    disable_publish: 'false'
+    disable_recent_gossip: 'false'
+    disable_historical_gossip: 'false'
+chc_namespace: null
 db_sync_strategy: Fast
 `
     fs.writeFileSync(conductorConfig.conductorConfigPath, conductorStringConfig);
