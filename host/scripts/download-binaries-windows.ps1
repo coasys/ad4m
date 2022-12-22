13259371e2812aa3f47d18f72a30697f5c4feb1e
Joshua Parkin <joshuadparkin@gmail.com>
2022-12-21 16:36:27 -0800

    chore: update to new binaries for ad4m host binary fetching

--- host/scripts/download-binaries-windows.ps1
+++ host/scripts/download-binaries-windows.ps1
@@ -2,6 +2,6 @@ choco install wget --no-progress
         
 mkdir temp/binary
 
-wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.0.174/hc-windows-0.0.69.exe -O ./temp/binary/hc.exe
+wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/hc-windows-0.1.0-beta-rc.1.exe -O ./temp/binary/hc.exe
 
-wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.0.174/holochain-windows-0.0.174.exe -O ./temp/binary/holochain.exe
+wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/holochain-windows-0.1.0-beta-rc.1.exe -O ./temp/binary/holochain.exe
