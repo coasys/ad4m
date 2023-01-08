d97b000ecada2f804639460192ddbf7936975b04
Nicolas Luck <nicolas@lucksus.eu>
2023-01-08 21:42:23 +0100

    Fix paths in download-binaries-windows.ps1

--- executor/scripts/download-binaries-windows.ps1
+++ executor/scripts/download-binaries-windows.ps1
@@ -2,9 +2,9 @@ choco install wget --no-progress
         
 mkdir temp/binary
 
-wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/hc-windows-0.1.0-beta-rc.1.exe -O ./hc.exe
+wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/hc-windows-0.1.0-beta-rc.1.exe -O ./temp/binary/hc.exe
 
-wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/holochain-windows-0.1.0-beta-rc.1.exe -O ./holochain.exe
+wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/holochain-windows-0.1.0-beta-rc.1.exe -O ./temp/binary/holochain.exe
 
 $SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
-Copy-Item $SwiplPath -Destination swipl.exe
+Copy-Item $SwiplPath -Destination ./temp/binary/swipl.exe
