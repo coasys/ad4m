8b16b5c1c10032883433a26b2694c3ebbe41aa27
Joshua Parkin <joshuadparkin@gmail.com>
2023-01-09 18:25:49 +0000

    check if ad4m is already running and return if so

--- ui/src-tauri/src/util.rs
+++ ui/src-tauri/src/util.rs
@@ -4,6 +4,7 @@ use crate::menu::open_logs_folder;
 use std::fs::remove_file;
 use std::fs::File;
 use std::io::prelude::*;
+use sysinfo::Process;
 use sysinfo::{ProcessExt, Signal, System, SystemExt};
 use tauri::{AppHandle, Manager, WindowBuilder, WindowEvent, WindowUrl, Wry};
 
@@ -32,6 +33,18 @@ pub fn find_and_kill_processes(name: &str) {
     }
 }
 
+pub fn has_process_running(name: &str) -> bool {
+    let processes = System::new_all();
+    let processes_by_name: Vec<&Process> = processes.processes_by_exact_name(name).collect();
+    let mut running = false;
+
+    for process in processes_by_name {
+        log::info!("Prosses running: {} {}", process.pid(), process.name());
+        running = true;
+    }
+    running
+}
+
 pub fn create_main_window(app: &AppHandle<Wry>) {
     let url = app_url();
 
@@ -49,7 +62,11 @@ pub fn create_main_window(app: &AppHandle<Wry>) {
     //let _ = tray_window.move_window(Position::TrayCenter);
 
     let _id = tray_window.listen("copyLogs", |event| {
-        log::info!("got window event-name with payload {:?} {:?}", event, event.payload());
+        log::info!(
+            "got window event-name with payload {:?} {:?}",
+            event,
+            event.payload()
+        );
 
         open_logs_folder();
     });
@@ -64,7 +81,6 @@ pub fn create_main_window(app: &AppHandle<Wry>) {
             }
         }
     });
-
 }
 
 pub fn save_executor_port(port: u16) {
