86fb3f973db74a041b0e7ddcc95578e75b8b7ad0
Nicolas Luck <nicolas@lucksus.eu>
2023-01-30 01:06:09 +0100

    Include --home in swipl wrapper script

--- executor/scripts/prepareTestDirectory.sh
+++ executor/scripts/prepareTestDirectory.sh
@@ -10,7 +10,9 @@ cp -r ../tests/test-language/build languages/test-language/build
 ln -s ../../temp/binary/hc ./hc
 ln -s ../../temp/binary/holochain ./holochain
 #ln -s ../../temp/swipl/bin/swipl ./swipl
+homedir=`echo "$(cd ../../temp/swipl/lib/swipl; pwd)"`
 echo '#!/bin/bash' > ./swipl
 echo 'cd ../../temp/swipl/bin' >> ./swipl
-echo './swipl "$@"' >> ./swipl
+echo -n "./swipl --home=${homedir} " >> ./swipl
+echo '"$@"' >> ./swipl
 chmod +x ./swipl
