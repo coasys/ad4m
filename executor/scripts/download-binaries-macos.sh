d9d49399ff873dfe38d6cc60bccfcde68341fea1
Joshua Parkin <joshuadparkin@gmail.com>
2023-02-01 14:42:25 +0000

    bump binaries to 0.1.0

--- executor/scripts/download-binaries-macos.sh
+++ executor/scripts/download-binaries-macos.sh
@@ -3,19 +3,19 @@
 [ ! -d "./temp/binary" ] && mkdir -p "./temp/binary"
 
 if [ ! -f "./temp/binary/hc" ]; then
-    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/hc-darwin-0.1.0-beta-rc.1
-    mv hc-darwin-0.1.0-beta-rc.1 ./temp/binary/hc
+    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps/hc-darwin
+    mv hc-darwin ./temp/binary/hc
     chmod +x ./temp/binary/hc
 fi
 
 if [ ! -f "./temp/binary/holochain" ]; then
-    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/holochain-darwin-0.1.0-beta-rc.1
-    mv holochain-darwin-0.1.0-beta-rc.1 ./temp/binary/holochain
+    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps/holochain-darwin
+    mv holochain-darwin ./temp/binary/holochain
     chmod +x ./temp/binary/holochain
 fi
 
 if [ ! -f "./temp/swipl/bin/swipl" ]; then
-    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0-beta-rc.1/swipl-darwin-x86.zip
+    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps/swipl-darwin-x86.zip
     unzip swipl-darwin-x86.zip -d ./temp
     rm -rf ./temp/__MACOSX
     rm -rf swipl-linux-x64.zip
