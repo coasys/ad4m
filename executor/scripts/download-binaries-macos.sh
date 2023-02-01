147d84fc9f0e55a9ce7b8e6b1d97bd32c4050258
Joshua Parkin <joshuadparkin@gmail.com>
2023-02-01 14:58:51 +0000

    fix missing version for macos and windows binaries

--- executor/scripts/download-binaries-macos.sh
+++ executor/scripts/download-binaries-macos.sh
@@ -3,19 +3,19 @@
 [ ! -d "./temp/binary" ] && mkdir -p "./temp/binary"
 
 if [ ! -f "./temp/binary/hc" ]; then
-    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps/hc-darwin
+    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/hc-darwin-0.1.0
     mv hc-darwin ./temp/binary/hc
     chmod +x ./temp/binary/hc
 fi
 
 if [ ! -f "./temp/binary/holochain" ]; then
-    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps/holochain-darwin
+    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/holochain-darwin-0.1.0
     mv holochain-darwin ./temp/binary/holochain
     chmod +x ./temp/binary/holochain
 fi
 
 if [ ! -f "./temp/swipl/bin/swipl" ]; then
-    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps/swipl-darwin-x86.zip
+    wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/swipl-darwin-x86.zip
     unzip swipl-darwin-x86.zip -d ./temp
     rm -rf ./temp/__MACOSX
     rm -rf swipl-linux-x64.zip
