276aaec38b3eda4b3f7c94c3cd325391de9f13ba
Nicolas Luck <nicolas@lucksus.eu>
2023-02-08 12:30:58 +0100

    Add missing file NeighbourhoodProxy

---
+++ core/src/neighbourhood/NeighbourhoodProxy.ts
@@ -0,0 +1,42 @@
+import { DID } from "../DID";
+import { OnlineAgent } from "../language/Language";
+import { Perspective } from "../perspectives/Perspective";
+import { NeighbourhoodClient } from "./NeighbourhoodClient";
+
+export class NeighbourhoodProxy {
+    #client: NeighbourhoodClient
+    #pID: string
+
+    constructor(client: NeighbourhoodClient, pID: string) {
+        this.#client = client
+        this.#pID = pID
+    }
+
+    async otherAgents(): Promise<DID[]> {
+        return await this.#client.otherAgents(this.#pID)
+    }
+
+    async hasTelepresenceAdapter(): Promise<boolean> {
+        return await this.#client.hasTelepresenceAdapter(this.#pID)
+    }
+
+    async onlineAgents(): Promise<OnlineAgent[]> {
+        return await this.#client.onlineAgents(this.#pID)
+    }
+
+    async setOnlineStatus(status: Perspective): Promise<boolean> {
+        return await this.#client.setOnlineStatus(this.#pID, status)
+    }
+
+    async sendSignal(remoteAgentDid: string, payload: Perspective): Promise<boolean> {
+        return await this.#client.sendSignal(this.#pID, remoteAgentDid, payload)
+    }
+
+    async sendBroadcast(payload: Perspective): Promise<boolean> {
+        return await this.#client.sendBroadcast(this.#pID, payload)
+    }
+
+    async addSignalHandler(handler: (payload: Perspective) => object): Promise<void> {
+        return await this.#client.addSignalHandler(this.#pID, handler)
+    }
+}
