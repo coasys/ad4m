9d0d91266d5356fa3db31efbe7fb89ec4035ef0f
Fayeed <fayeed@live.com>
2023-02-23 14:29:39 +0530

    Expression tests updated

--- test-runner/example/expression.test.js
+++ test-runner/example/expression.test.js
@@ -1,12 +1,14 @@
-const { createExpression, getExpression } = require('@perspect3vism/ad4m-test/helpers')
+const { createExpression, getExpression, spawnExpressionAgent  } = require('@perspect3vism/ad4m-test/helpers')
 
 describe("Expression", () => {
   it("Create Expression", async () => {
-    const exp = await createExpression("{\"name\": \"hello world!\"}");
+    const agent = await spawnExpressionAgent()
+
+    const exp = await agent.create("{\"name\": \"hello world!\"}");
 
     expect(exp).not.toBeNull()
 
-    const fetched = await getExpression(exp);
+    const fetched = await agent.get(exp)
 
     expect(fetched).not.toBeNull()
   })
