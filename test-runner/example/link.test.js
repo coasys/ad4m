0da487104393e66a2945cdc5f197269db9e06b69
Fayeed <fayeed@live.com>
2023-02-23 14:30:44 +0530

    Updated link language tests

--- test-runner/example/link.test.js
+++ test-runner/example/link.test.js
@@ -1,11 +1,14 @@
-const { addLink, updateLink, removeLink, queryLinks } = require('@perspect3vism/ad4m-test/helpers')
+const { spawnLinkAgent } = require('@perspect3vism/ad4m-test/helpers')
 
 describe("Link", () => {
   it("Create Link", async () => {
-    const all = await queryLinks({});
+    const agent = await spawnLinkAgent();
+
+    const all = await agent.queryLinks({});
 
     expect(all.length).toBe(0)
-    const link = await addLink({source:"root", predicate: "soic://test", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
+    
+    const link = await agent.addLink({source:"root", predicate: "soic://test", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
 
     const all1 = await queryLinks({});
 
@@ -16,38 +19,42 @@ describe("Link", () => {
   });
 
   it("Remove Link", async () => {
-    const all = await queryLinks({});
+    const agent = await spawnLinkAgent();
+
+    const all = await agent.queryLinks({});
 
     expect(all.length).toBe(0)
-    const link = await addLink({source:"root", predicate: "soic://test", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
+    const link = await agent.addLink({source:"root", predicate: "soic://test", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
 
-    const all1 = await queryLinks({});
+    const all1 = await agent.queryLinks({});
 
     expect(all1.length).toBe(1)
     
-    const removedlink = await removeLink(link)
+    const removedlink = await agent.removeLink(link)
 
-    const all2 = await queryLinks({});
+    const all2 = await agent.queryLinks({});
 
     expect(all2.length).toBe(0)
   });
 
   it("Update Link", async () => {
-    const all = await queryLinks({});
+    const agent = await spawnLinkAgent();
+
+    const all = await agent.queryLinks({});
 
     expect(all.length).toBe(0)
-    const link = await addLink({source:"root", predicate: "soic://test", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
+    const link = await agent.addLink({source:"root", predicate: "soic://test", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
 
-    const all1 = await queryLinks({});
+    const all1 = await agent.queryLinks({});
 
     expect(all1.length).toBe(1)
     expect(all1[0].data.source).toBe(link.data.source)
     expect(all1[0].data.predicate).toBe(link.data.predicate)
     expect(all1[0].data.target).toBe(link.data.target)
 
-    const newLink = await updateLink(link, {source:"root", predicate: "soic://test1", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
+    const newLink = await agent.updateLink(link, {source:"root", predicate: "soic://test1", target:"QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL://QmSsCCtXMDAZXMpyiNLzwjGEU4hLmhG7fphidhEEodQ4Wy"})
   
-    const all2 = await queryLinks({});
+    const all2 = await agent.queryLinks({});
 
     expect(all2.length).toBe(1)
     expect(all2[0].data.source).toBe(newLink.data.source)
