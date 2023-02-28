a3d0db019bf6b013add0bb210ee30d0aef6da005
Joshua Parkin <joshuadparkin@gmail.com>
2023-02-28 17:07:03 +0000

    add simple explainer and flux link if no apps connected

--- ui/src/components/Apps.tsx
+++ ui/src/components/Apps.tsx
@@ -40,42 +40,51 @@ const Apps = () => {
 
   return (
     <div style={MainContainer}>
-      <div style={{ ...listStyle }}>
-        {apps.map((app, index) => (
-          <div key={`app-${index}`} style={{ ...cardStyle, width: "100%" }}>
-            <j-flex a="flex-start" direction="column">
-              <j-flex direction="column" style={{ marginTop: 4 }}>
-                <j-text variant="bold" size="600">
-                  {app.auth.appName}
-                  &nbsp;&nbsp;
-                  {app.revoked && <j-badge variant="warning">revoked</j-badge>}
-                </j-text>
-                <j-box p="200"></j-box>
-                <j-text size="400">{app.auth.appDesc}</j-text>
-                <a
-                  style={{ ...linkStyle }}
-                  href={app.auth.appUrl}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {app.auth.appUrl}
-                </a>
+      {apps.length === 0 ? (
+        <div>
+          <j-text variant="heading">Welcome to AD4M, here you can manage your connected applications</j-text>
+          <br></br>
+          <j-text variant="heading-sm">You have no apps connected...</j-text>
+          <j-text variant="body">Try Flux <a href="https://app.fluxsocial.io"  target="_blank">here</a> to get started</j-text>
+        </div>
+      ) : (
+        <div style={{ ...listStyle }}>
+          {apps.map((app, index) => (
+            <div key={`app-${index}`} style={{ ...cardStyle, width: "100%" }}>
+              <j-flex a="flex-start" direction="column">
+                <j-flex direction="column" style={{ marginTop: 4 }}>
+                  <j-text variant="bold" size="600">
+                    {app.auth.appName}
+                    &nbsp;&nbsp;
+                    {app.revoked && <j-badge variant="warning">revoked</j-badge>}
+                  </j-text>
+                  <j-box p="200"></j-box>
+                  <j-text size="400">{app.auth.appDesc}</j-text>
+                  <a
+                    style={{ ...linkStyle }}
+                    href={app.auth.appUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {app.auth.appUrl}
+                  </a>
+                </j-flex>
+                <div style={{ position: "absolute", top: 0, right: 0 }}>
+                  <j-button
+                    variant="link"
+                    onClick={() => {
+                      setShowModal(true);
+                      setSelectedRequestId(app.requestId);
+                    }}
+                  >
+                    <j-icon name="x" />
+                  </j-button>
+                </div>
               </j-flex>
-              <div style={{ position: "absolute", top: 0, right: 0 }}>
-                <j-button
-                  variant="link"
-                  onClick={() => {
-                    setShowModal(true);
-                    setSelectedRequestId(app.requestId);
-                  }}
-                >
-                  <j-icon name="x" />
-                </j-button>
-              </div>
-            </j-flex>
-          </div>
-        ))}
-      </div>
+            </div>
+          ))}
+        </div>
+      )} 
       {showModal && (
         <j-modal
           size="fullscreen"
