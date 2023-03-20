7291a970543810af8e3bd9a0c4791e90c8533af9
Joshua Parkin <joshuadparkin@gmail.com>
2023-03-20 15:16:07 +0000

    remove swipl install for windows binary installs

--- download-binaries-windows.ps1
+++ download-binaries-windows.ps1
@@ -6,9 +6,6 @@ wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/h
 
 wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/holochain-windows-0.1.0.exe -O ./executor/temp/binary/holochain.exe
 
-$SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
-Copy-Item $SwiplPath -Destination ./executor/temp/binary/swipl.exe
-
 # Global path where the binary should be copied to
 $global_path = "C:\Windows\System32\"
 
