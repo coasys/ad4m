01e2d164eb284e9509ef9ec3c010dc0a3b7b035b
Fayeed <fayeed@live.com>
2023-03-31 11:44:59 +0530

    get-language script updated

--- test-runner/scripts/get-builtin-test-langs.js
+++ test-runner/scripts/get-builtin-test-langs.js
@@ -1,6 +1,4 @@
 const fs = require("fs-extra");
-const wget = require("node-wget-js");
-const { Extract } = require("unzipper");
 const { join } = require("path");
 
 const languages = {
@@ -8,16 +6,16 @@ const languages = {
     bundle: "../bootstrap-languages/agent-language/build/bundle.js",
   },
   languages: {
-      bundle: "https://github.com/perspect3vism/local-language-persistence/releases/download/0.0.5/bundle.js",
+      bundle: "../bootstrap-languages/local-language-persistence/build/bundle.js",
   },
   "neighbourhood-store": {
-    bundle: "https://github.com/perspect3vism/local-neighbourhood-persistence/releases/download/0.0.2/bundle.js",
+    bundle: "../bootstrap-languages/local-neighbourhood-persistence/build/bundle.js",
   },
   "perspective-diff-sync": {
-    bundle: "../bootstrap-languages/perspective-diff-sync/build/bundle.js",
+    bundle: "../bootstrap-languages/p-diff-sync/build/bundle.js",
   },
   "note-ipfs": {
-    bundle: "https://github.com/perspect3vism/lang-note-ipfs/releases/download/0.0.4/bundle.js",
+    bundle: "../bootstrap-languages/note-ipfs/build/bundle.js",
   },
   "direct-message-language": {
     bundle: "../bootstrap-languages/direct-message-language/build/bundle.js",
@@ -40,45 +38,8 @@ async function main() {
     if (languages[lang].bundle) {
       url = languages[lang].bundle;
       dest = dir + "/build/bundle.js";
-      if (url.slice(0, 8) != "https://" && url.slice(0, 7) != "http://") {
-        fs.copyFileSync(url, dest);
-      } else {
-        wget({ url, dest });
-      }
-    }
-    
-    // dna
-    if (languages[lang].dna) {
-      console.log(languages[lang])
-      url = languages[lang].dna;
-      dest = dir + `/${languages[lang].targetDnaName}.dna`;
-      wget({ url, dest });
-    }
 
-    if (languages[lang].zipped) {
-      await wget(
-        {
-          url: languages[lang].resource,
-          dest: `${dir}/lang.zip`,
-        },
-        async () => {
-          //Read the zip file into a temp directory
-          await fs.createReadStream(`${dir}/lang.zip`)
-            .pipe(Extract({ path: `${dir}` }))
-            .promise();
-
-          // if (!fs.pathExistsSync(`${dir}/bundle.js`)) {
-          //   throw Error("Did not find bundle file in unzipped path");
-          // }
-
-          fs.copyFileSync(
-            join(`${dir}/bundle.js`),
-            join(`${dir}/build/bundle.js`)
-          );
-          fs.rmSync(`${dir}/lang.zip`);
-          fs.rmSync(`${dir}/bundle.js`);
-        }
-      );
+      fs.copyFileSync(url, dest);
     }
   }
 }
