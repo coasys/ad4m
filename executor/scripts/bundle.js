5d583918c9fabfc51ceb1eea1539c3b7d8425c3c
Nicolas Luck <nicolas@lucksus.eu>
2023-04-04 12:28:40 +0100

    WIP: include deno std lib instead of node

--- executor/scripts/bundle.js
+++ executor/scripts/bundle.js
@@ -1,4 +1,39 @@
 import esbuild from 'esbuild'
+import { polyfillNodeForDeno } from "esbuild-plugin-polyfill-node";
+import path from 'path'
+
+function denoAlias(nodeModule) {
+    return {
+        name: `${nodeModule}-alias`,
+        setup(build) {
+            build.onResolve({ filter: new RegExp(`^${nodeModule}$`) }, (args) => {
+                return { path: `https://deno.land/std@0.177.0/${nodeModule}/mod.ts`, external: true };
+            });
+        },
+    }
+}
+
+function denoAliasLocal(nodeModule) {
+    return {
+        name: `${nodeModule}-alias`,
+        setup(build) {
+            build.onResolve({ filter: new RegExp(`^${nodeModule}$`) }, (args) => {
+                return { path: path.resolve(`deno_std-0.177.0/node/${nodeModule}.ts`), external: false };
+            });
+        },
+    }
+}
+
+function denoAliasNode(nodeModule) {
+    return {
+        name: `${nodeModule}-alias`,
+        setup(build) {
+            build.onResolve({ filter: new RegExp(`^${nodeModule}$`) }, (args) => {
+                return { path: `node:${nodeModule}`, external: true };
+            });
+        },
+    }
+}
 
 esbuild
   .build({
@@ -8,7 +43,23 @@ esbuild
     platform: 'node',
     target: 'es2020',
     format: 'esm',
-    plugins: [],
+    plugins: [
+        ...[
+            'crypto', 'path', 'fs', 'child_process', 'net', 'dns', 'cluster', 'https',
+            'dgram', 'os', 'tls', 'http', 'url', 'util', 'stream', 'events', 'tty',
+            'zlib', 'assert', 'buffer', 'constants', 'querystring', 'string_decoder',
+        ].map(denoAliasLocal),
+        {
+            name: `dns-promisis-alias`,
+            setup(build) {
+                build.onResolve({ filter: new RegExp(`^dns/promises$`) }, (args) => {
+                    return { path: path.resolve(`deno_std-0.177.0/node/dns.ts`), external: false };
+                });
+            },
+        },
+        //polyfillNodeForDeno(),
+    ]
+    
   })
   .catch((error) => {
     console.error(error);
