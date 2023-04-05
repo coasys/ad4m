fe0b588ce6f5ece08d625d9ed0459ec3ebc2f104
Nicolas Luck <nicolas@lucksus.eu>
2023-04-05 12:07:52 +0100

    WIP: providing config to executor init

--- executor/lib/deno.js
+++ executor/lib/deno.js
@@ -7,5 +7,10 @@ const setImmediate = globalThis.setImmediate;
 const clearImmediate = globalThis.clearImmediate;
 
 import { init as internalInit } from "./main.js"
+import * as internalPath from "../deno_std-0.177.0/node/path.ts";
+import * as internalOs from "../deno_std-0.177.0/node/os.ts"
 console.log("Hello from deno.js")
 export const init = internalInit
+export const path = internalPath
+export const os = internalOs
+
--- rust-executor/src/main.js
+++ rust-executor/src/main.js
@@ -1,4 +1,42 @@
-import { init } from 'https://ad4m.runtime/executor'
+import { init, path, os } from 'https://ad4m.runtime/executor'
 
 console.log("Hello from main")
-init()
+
+let appDataPath = path.join(os.homedir(), 'ad4m', 'tests', 'ad4m1');
+const binaryPath = path.join(appDataPath, 'binary');
+const swiplHomePath = (process.platform == "win32" ? path.join(appDataPath, 'swipl/') : path.join(appDataPath, 'swipl/lib/swipl/'))
+const swiplPath = path.join(appDataPath, 'swipl/bin/swipl');
+const gqlPort = 13000
+const ipfsRepoPath = path.join(appDataPath, 'ipfs')
+
+//if (!fs.existsSync(appDataPath)) {
+//  fs.mkdirSync(appDataPath);
+//}
+//const bLanguage = bootstrapLanguage ? await import(path.isAbsolute(bootstrapLanguage) ? bootstrapLanguage: path.join(__dirname, bootstrapLanguage)) : [];
+//const bPerspective = bootstrapPerspective ? await import(path.isAbsolute(bootstrapPerspective) ? bootstrapPerspective: path.join(__dirname, bootstrapPerspective)) : [];
+
+const config = {
+appDataPath: appDataPath,
+resourcePath: binaryPath,
+networkBootstrapSeed: appDataPath,
+languageLanguageOnly: true,
+bootstrapFixtures: {
+    languages: [],
+    perspectives: [],
+},
+appLangAliases: {},
+mocks: false,
+runDappServer: false,
+gqlPort,
+hcPortAdmin: undefined,
+hcPortApp: undefined,
+ipfsRepoPath,
+ipfsSwarmPort: undefined,
+connectHolochain: true,
+reqCredential: undefined,
+swiplPath,
+swiplHomePath
+};
+
+const core = await init(config)
+console.log(core)
