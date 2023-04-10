3f68ac40f804131bd0ee9521a9678118175bcca8
Joshua Parkin <joshuadparkin@gmail.com>
2023-04-10 20:00:36 +0100

    get wallet code compiling + test

--- rust-executor/src/wallet.rs
+++ rust-executor/src/wallet.rs
@@ -1,67 +1,109 @@
-use std::collections::BTreeMap;
-use std::sync::{Arc, Mutex};
-use secp256k1::ecdsa::Signature;
-use secp256k1::{
-    PublicKey, SecretKey,
-};
-use secp256k1::rand::rngs::OsRng;
-use secp256k1::{Message};
+use argon2::password_hash::Salt;
+use argon2::{self, Argon2, PasswordHasher};
+use base64::Engine;
+use crypto_box::aead::{Aead, OsRng};
+use crypto_box::{Nonce, PublicKey as cPublicKey, SalsaBox, SecretKey as cSecretKey};
+use deno_core::anyhow::anyhow;
+use deno_core::error::AnyError;
 use lazy_static::lazy_static;
-use crypto_box::{SalsaBox, SecretKey as cSecretKey, PublicKey as cPublicKey};
-use argon2::{self, PasswordHasher, Argon2};
-use argon2::password_hash::{Salt};
-use rand::Rng;
-use serde::{Serialize, Deserialize};
-use serde_json::Result;
-use crypto_box::aead::{Aead, Nonce};
+use secp256k1::ecdsa::Signature;
+use secp256k1::Message;
+use secp256k1::{PublicKey, SecretKey};
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::convert::TryInto;
+use std::sync::{Arc, Mutex};
+
+fn slice_to_u8_array(slice: &[u8]) -> Option<[u8; 32]> {
+    //If length of slice is not 32 then take the first 32 bytes
+    let slice_32 = if slice.len() != 32 {
+        let mut array: [u8; 32] = [0u8; 32];
+        let mut i = 0;
+        for byte in slice {
+            if i == 32 {
+                break;
+            }
+            array[i] = *byte;
+            i += 1;
+        }
+        array
+    } else {
+        let array: [u8; 32] = slice.try_into().expect("slice with incorrect length");
+        array
+    };
+
+    let array: Result<[u8; 32], _> = slice_32.try_into();
+    array.ok()
+}
 
 fn encrypt(payload: String, passphrase: String) -> String {
     let salt = Salt::from_b64("0000000000000000").expect("salt from zeros to work");
+
     // Derive secret key from passphrase
     let argon2 = Argon2::default();
-    let derived_secret_key = argon2.hash_password(passphrase.as_bytes(), salt).unwrap();
-    let derived_secret_key_string = derived_secret_key.to_string().as_str();
-    let secret_key = cSecretKey::from(derived_secret_key_string.as_bytes().clone().take(...10).unwrap().into());
-    let public_key = cPublicKey::from(secret_key.into());
+    //NOTE: we need to be sure to enforce min password size so we ensure that we will always get 32 bytes to work from
+    let derived_secret_key = argon2
+        .hash_password(passphrase.as_bytes(), salt)
+        .unwrap()
+        .to_string();
+    let derived_secret_key_bytes = derived_secret_key.as_bytes();
+    let secret_key = cSecretKey::from(
+        slice_to_u8_array(derived_secret_key_bytes).expect("Could not slice to u8 array"),
+    );
+    let public_key = cPublicKey::from(&secret_key);
 
     // Create the Box (encryptor/decryptor) using the derived secret key and the public key
     let crypto_box = SalsaBox::new(&public_key, &secret_key);
 
-    let nonce = Nonce::from("000000000".into());
-    // Encrypt
-    let encrypted_data = crypto_box.encrypt(&nonce, payload.into()).unwrap();
+    //let nonce = SalsaBox::generate_nonce(&mut OsRng);
+    let nonce = Nonce::default();
 
-    String::from_utf8(encrypted_data).expect("encrypted array to be a string")
+    // Encrypt
+    let encrypted_data = crypto_box.encrypt(&nonce, payload.as_bytes()).unwrap();
+
+    base64::engine::general_purpose::STANDARD_NO_PAD.encode(encrypted_data)
 }
 
-fn decrypt(payload: String, passphrase: String) -> Result<String> {
-    // Salt for Argon2 key derivation function
-    let mut salt = [0u8; 16];
-    let mut rng = rand::thread_rng();
-    rng.fill(&mut salt);
+fn decrypt(payload: String, passphrase: String) -> Result<String, crypto_box::aead::Error> {
+    let salt = Salt::from_b64("0000000000000000").expect("salt from zeros to work");
 
     // Derive secret key from passphrase
     let argon2 = Argon2::default();
-    let derived_secret_key = argon2.hash_password(passphrase.as_bytes(), &salt).unwrap();
-    let secret_key = cSecretKey::from_slice(&derived_secret_key[0..32]).unwrap();
-    let public_key = cPublicKey::from(secret_key);
+    let derived_secret_key = argon2
+        .hash_password(passphrase.as_bytes(), salt)
+        .unwrap()
+        .to_string();
+    let derived_secret_key_bytes = derived_secret_key.as_bytes();
+    let secret_key = cSecretKey::from(
+        slice_to_u8_array(derived_secret_key_bytes).expect("Could not slice to u8 array"),
+    );
+    let public_key = cPublicKey::from(&secret_key);
 
     // Create the Box (encryptor/decryptor) using the derived secret key and the public key
     let crypto_box = SalsaBox::new(&public_key, &secret_key);
 
+    //Pretty sure this not gonna work since this will be a different nonce to what is generated on encrypt
+    let nonce = Nonce::default();
+
+    let payload_bytes = base64::engine::general_purpose::STANDARD_NO_PAD
+        .decode(payload.as_bytes())
+        .expect("Could not decode payload");
+
     // Decrypt
-    let decrypted_data = crypto_box.decrypt(&payload);
+    let decrypted_data = crypto_box
+        .decrypt(&nonce, payload_bytes.as_slice())
+        .map(|data| String::from_utf8(data).expect("decrypted array to be a string"));
 
     decrypted_data
 }
 
-
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Key {
     pub secret: SecretKey,
     pub public: PublicKey,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Keys {
     keys: BTreeMap<String, Key>,
 }
@@ -100,29 +142,32 @@ impl Wallet {
         }
         let secp = secp256k1::Secp256k1::new();
         let (secret, public) = secp.generate_keypair(&mut OsRng);
-        self.keys.unwrap().insert(name, Key { secret, public });
+        self.keys
+            .clone()
+            .unwrap()
+            .insert(name, Key { secret, public });
     }
 
     pub fn get_public_key(&self, name: String) -> Option<PublicKey> {
-        self.keys?.get(&name).map(|key| key.public)
+        self.keys.clone()?.get(&name).map(|key| key.public)
     }
 
     pub fn get_secret_key(&self, name: String) -> Option<SecretKey> {
-        self.keys?.get(&name).map(|key| key.secret)
+        self.keys.clone()?.get(&name).map(|key| key.secret)
     }
 
     pub fn sign(&self, name: String, message: &[u8]) -> Option<Signature> {
         // hash the message
         let message = Message::from_slice(message).expect("32 bytes");
         // sign the hash
-        self.keys?.get(&name).map(|key| {
+        self.keys.clone()?.get(&name).map(|key| {
             let secp = secp256k1::Secp256k1::new();
             secp.sign_ecdsa(&message, &key.secret)
         })
     }
 
     pub fn lock(&mut self, passphrase: String) {
-        if let Some(&keys) = self.keys {
+        if let Some(keys) = &self.keys {
             let string = serde_json::to_string(&keys).unwrap();
             let encrypted = encrypt(string, passphrase);
             self.cipher = Some(encrypted);
@@ -130,10 +175,11 @@ impl Wallet {
         }
     }
 
-    pub fn unlock(&mut self, passphrase: String) -> Result<()> {
-        let string = decrypt(self.cipher, passphrase)?;
+    pub fn unlock(&mut self, passphrase: String) -> Result<(), AnyError> {
+        let string = decrypt(self.cipher.clone().expect("No cypher selected"), passphrase)
+            .map_err(|err| anyhow!(err))?;
         let keys: Keys = serde_json::from_str(&string)?;
-        self.keys = Some(keys);
+        self.keys = Some(keys.keys);
         Ok(())
     }
 
@@ -142,10 +188,10 @@ impl Wallet {
     }
 
     pub fn export(&mut self, passphrase: String) -> String {
-        if let Some(&keys) = self.keys {
-            let string = serde_json::to_string(&keys).unwrap();
+        if let Some(keys) = &self.keys {
+            let string = serde_json::to_string(keys).unwrap();
             let encrypted = encrypt(string, passphrase);
-            self.cipher = Some(encrypted);
+            self.cipher = Some(encrypted.clone());
             encrypted
         } else {
             String::new()
@@ -155,5 +201,29 @@ impl Wallet {
     pub fn load(&mut self, data: String) {
         self.cipher = Some(data);
     }
+}
 
+#[cfg(test)]
+mod tests {
+    //Test the encryption and decryption of a string
+    use super::*;
+
+    #[test]
+    fn test_encrypt_decrypt_multiple() {
+        let passphrase = "test".to_string();
+        let payload = "test".to_string();
+        let encrypted = encrypt(payload.clone(), passphrase.clone());
+        println!("Got encrypted: {}", encrypted);
+        let decrypted = decrypt(encrypted, passphrase);
+        println!("Got decrypted: {:?}", decrypted);
+        assert_eq!(payload, decrypted.unwrap());
+
+        let passphrase = "test".to_string();
+        let payload = "test".to_string();
+        let encrypted = encrypt(payload.clone(), passphrase.clone());
+        println!("Got encrypted: {}", encrypted);
+        let decrypted = decrypt(encrypted, passphrase);
+        println!("Got decrypted: {:?}", decrypted);
+        assert_eq!(payload, decrypted.unwrap());
+    }
 }
