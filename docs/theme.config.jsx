667b0fea849c391191f8e2449a6a37ba0a5e7fb1
Bård N. Hovde <bnhovde@gmail.com>
2023-04-18 13:49:38 +0200

    Fixed edit page url

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -3,7 +3,7 @@ export default {
       link: 'https://github.com/perspect3vism/ad4m',
     },
     logo: <><img src="/images/logo.svg" style={{ marginRight: '10px' }} />AD4M Docs</>,
-    docsRepositoryBase: 'https://github.com/perspect3vism/ad4m',
+    docsRepositoryBase: 'https://github.com/perspect3vism/ad4m/blob/dev/docs/',
     footer: {
         text: "AD4M - The first social network"
     },
