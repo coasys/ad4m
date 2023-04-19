52714e456d1b0076567dc671b8db6deebd785881
Bård N. Hovde <bnhovde@gmail.com>
2023-04-19 18:06:46 +0200

    Unignore next.config.js

---
+++ docs/next.config.js
@@ -0,0 +1,12 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra({
+  images: { unoptimized: true },
+  output: "export",
+});
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
