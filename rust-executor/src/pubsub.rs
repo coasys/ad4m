50c1da81f9374d0d9b906f2d2e2defba5bff567d
Joshua Parkin <joshuadparkin@gmail.com>
2023-04-21 15:27:53 +0100

    fix subscription topic names

--- rust-executor/src/pubsub.rs
+++ rust-executor/src/pubsub.rs
@@ -54,15 +54,12 @@ pub(crate) async fn subscribe_and_process<T: DeserializeOwned + Send + 'static>(
     let receiver = pubsub.subscribe(&topic).await;
     let receiver_stream = WatchStream::from_changes(receiver);
 
-    let mapped_stream = receiver_stream.map(|msg| {
-        println!("subscribe_and_process: Received message: {:?}", msg);
-        match serde_json::from_str::<T>(&msg) {
-            Ok(agent_status) => Ok(agent_status),
-            Err(e) => Err(FieldError::new(
-                e,
-                graphql_value!({ "type": "INTERNAL_ERROR_COULD_NOT_SERIALIZE" }),
-            )),
-        }
+    let mapped_stream = receiver_stream.map(|msg| match serde_json::from_str::<T>(&msg) {
+        Ok(agent_status) => Ok(agent_status),
+        Err(e) => Err(FieldError::new(
+            e,
+            graphql_value!({ "type": "INTERNAL_ERROR_COULD_NOT_SERIALIZE" }),
+        )),
     });
 
     Box::pin(mapped_stream)
@@ -71,18 +68,18 @@ pub(crate) async fn subscribe_and_process<T: DeserializeOwned + Send + 'static>(
 lazy_static::lazy_static! {
     static ref GLOBAL_PUB_SUB: Arc<PubSub> = Arc::new(PubSub::new());
 
-    pub static ref AGENT_STATUS_CHANGED_TOPIC: String = "agent-status-changed".to_owned();
-    pub static ref AGENT_UPDATED_TOPIC: String = "agent-updated".to_owned();
-    pub static ref EXCEPTION_OCCURRED_TOPIC: String = "exception-occurred".to_owned();
-    pub static ref NEIGHBOURHOOD_SIGNAL_TOPIC: String = "neighbourhood-signal".to_owned();
-    pub static ref PERSPECTIVE_ADDED_TOPIC: String = "perspective-added".to_owned();
-    pub static ref PERSPECTIVE_LINK_ADDED_TOPIC: String = "perspective-link-added".to_owned();
-    pub static ref PERSPECTIVE_LINK_REMOVED_TOPIC: String = "perspective-link-removed".to_owned();
-    pub static ref PERSPECTIVE_LINK_UPDATED_TOPIC: String = "perspective-link-updated".to_owned();
-    pub static ref PERSPECTIVE_REMOVED_TOPIC: String = "perspective-removed".to_owned();
-    pub static ref PERSPECTIVE_UPDATED_TOPIC: String = "perspective-updated".to_owned();
-    pub static ref PERSPECTIVE_SYNC_STATE_CHANGE_TOPIC: String = "perspective-sync-state-change".to_owned();
-    pub static ref RUNTIME_MESSAGED_RECEIVED_TOPIC: String = "runtime-messaged-received".to_owned();
+    pub static ref AGENT_STATUS_CHANGED_TOPIC: String = "agent-status-changed-topic".to_owned();
+    pub static ref AGENT_UPDATED_TOPIC: String = "agent-updated-topic".to_owned();
+    pub static ref EXCEPTION_OCCURRED_TOPIC: String = "exception-occurred-topic".to_owned();
+    pub static ref NEIGHBOURHOOD_SIGNAL_TOPIC: String = "neighbourhood-signal-topic".to_owned();
+    pub static ref PERSPECTIVE_ADDED_TOPIC: String = "perspective-added-topic".to_owned();
+    pub static ref PERSPECTIVE_LINK_ADDED_TOPIC: String = "perspective-link-added-topic".to_owned();
+    pub static ref PERSPECTIVE_LINK_REMOVED_TOPIC: String = "perspective-link-removed-topic".to_owned();
+    pub static ref PERSPECTIVE_LINK_UPDATED_TOPIC: String = "perspective-link-updated-topic".to_owned();
+    pub static ref PERSPECTIVE_REMOVED_TOPIC: String = "perspective-removed-topic".to_owned();
+    pub static ref PERSPECTIVE_UPDATED_TOPIC: String = "perspective-updated-topic".to_owned();
+    pub static ref PERSPECTIVE_SYNC_STATE_CHANGE_TOPIC: String = "perspective-sync-state-change-topic".to_owned();
+    pub static ref RUNTIME_MESSAGED_RECEIVED_TOPIC: String = "runtime-messaged-received-topic".to_owned();
 }
 
 pub async fn get_global_pubsub() -> Arc<PubSub> {
