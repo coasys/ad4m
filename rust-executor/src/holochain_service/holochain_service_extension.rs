aea78069ac1bb7db04b0f34baac0ccddcae0f5fe
Joshua Parkin <joshuadparkin@gmail.com>
2023-05-18 16:34:10 +0100

    integrate holochain service extension with new service interface

--- rust-executor/src/holochain_service/holochain_service_extension.rs
+++ rust-executor/src/holochain_service/holochain_service_extension.rs
@@ -10,32 +10,33 @@ use log::info;
 
 use crate::holochain_service::{HolochainService, LocalConductorConfig};
 
-use super::get_global_conductor;
+use super::get_holochain_service;
 
 #[op]
 async fn start_holochain_conductor(config: LocalConductorConfig) -> Result<(), AnyError> {
-    HolochainService::new(config).await?;
+    HolochainService::init(config).await?;
     Ok(())
 }
 
 #[op]
 async fn log_dht_status() -> Result<(), AnyError> {
-    let conductor = get_global_conductor().await;
-    let dht_status = conductor.conductor.dump_network_metrics(None).await?;
-    info!("DHT Status: {:#?}", dht_status);
+    let interface = get_holochain_service().await;
+    //let dht_status = interface.interface.dump_network_metrics(None).await?;
+    //info!("DHT Status: {:#?}", dht_status);
+    info!("DHT INFO UNIMPLEMENTED");
     Ok(())
 }
 
 #[op]
 async fn install_app(install_app_payload: InstallAppPayload) -> Result<AppInfo, AnyError> {
-    let mut conductor = get_global_conductor().await;
-    conductor.install_app(install_app_payload).await
+    let interface = get_holochain_service().await;
+    interface.install_app(install_app_payload).await
 }
 
 #[op]
 async fn get_app_info(app_id: String) -> Result<Option<AppInfo>, AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.get_app_info(app_id).await
+    let interface = get_holochain_service().await;
+    interface.get_app_info(app_id).await
 }
 
 //TODO
@@ -48,46 +49,46 @@ async fn call_zome_function(
     fn_name: String,
     payload: Option<ExternIO>,
 ) -> Result<ZomeCallResponse, AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor
+    let interface = get_holochain_service().await;
+    interface
         .call_zome_function(app_id, cell_name, zome_name, fn_name, payload)
         .await
 }
 
 #[op]
 async fn agent_infos() -> Result<Vec<AgentInfoSigned>, AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.agent_infos().await
+    let interface = get_holochain_service().await;
+    interface.agent_infos().await
 }
 
 #[op]
 async fn add_agent_infos(agent_infos_payload: Vec<AgentInfoSigned>) -> Result<(), AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.add_agent_infos(agent_infos_payload).await
+    let interface = get_holochain_service().await;
+    interface.add_agent_infos(agent_infos_payload).await
 }
 
 #[op]
 async fn remove_app(app_id: String) -> Result<(), AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.remove_app(app_id).await
+    let interface = get_holochain_service().await;
+    interface.remove_app(app_id).await
 }
 
 #[op]
 async fn sign_string(data: String) -> Result<Signature, AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.sign(data).await
+    let interface = get_holochain_service().await;
+    interface.sign(data).await
 }
 
 #[op]
 async fn shutdown() -> Result<(), AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.shutdown().await
+    let interface = get_holochain_service().await;
+    interface.shutdown().await
 }
 
 #[op]
 async fn get_agent_key() -> Result<HoloHash<Agent>, AnyError> {
-    let conductor = get_global_conductor().await;
-    conductor.get_agent_key().await
+    let interface = get_holochain_service().await;
+    interface.get_agent_key().await
 }
 
 //TODO: implement dna packing and unpacking (not currently possible with holochain_cli_bundle unpack / pack functions since it does not exposed the functions in lib)
