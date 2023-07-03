16007640de986dbe30421b5cfef49ac80f4e98ff
Joshua Parkin <joshuadparkin@gmail.com>
2023-07-03 08:37:28 +0100

    get node libraries in import map

--- executor/esbuild.ts
+++ executor/esbuild.ts
@@ -23,60 +23,6 @@ const result = await esbuild.build({
     charset: 'ascii',
     legalComments: 'inline',
     plugins: [
-        ...[
-            'crypto', 'path', 'fs', 'net', 'dns', 'cluster', 'https',
-            'dgram', 'os', 'tls', 'http', 'url', 'util', 'stream', 'events', 'tty',
-            'zlib', 'assert', 'buffer', 'constants', 'querystring', 'string_decoder',
-            'global', 'process', 
-        ].map(denoAlias),
-        {
-            name: `dns-promisis-alias`,
-            setup(build) {
-                build.onResolve({ filter: new RegExp(`^dns/promises$`) }, (args) => {
-                    return { path: `https://deno.land/std@0.177.0/node/dns.ts`, external: true };
-                });
-            },
-        },
-        {
-            name: `child_process`,
-            setup(build) {
-                build.onResolve({ filter: new RegExp(`^child_process$`) }, (args) => {
-                    return { path: `https://deno.land/std@0.177.0/node/child_process.ts`, external: true };
-                });
-            },
-        },
-        {
-            name: `fs-promisis-alias`,
-            setup(build) {
-                build.onResolve({ filter: new RegExp(`^fs/promises$`) }, (args) => {
-                    return { path: `https://deno.land/std@0.177.0/node/fs.ts`, external: true };
-                });
-            },
-        },
-        {
-            name: `ws-alias`,
-            setup(build) {
-                build.onResolve({ filter: new RegExp(`^ws$`) }, (args) => {
-                    return { path: `https://deno.land/x/websocket@v0.1.4/mod.ts`, external: true };
-                });
-            },
-        },
-        {
-            name: `aloe`,
-            setup(build) {
-                build.onResolve({ filter: new RegExp(`^aloedb-node$`) }, (args) => {
-                    return { path: 'https://deno.land/x/aloedb@0.9.0/mod.ts', external: true };
-                });
-            },
-        },
-        {
-            name: "https://deno.land/std@0.150.0/media_types/mod.ts",
-            setup(build) {
-                build.onResolve({ filter: new RegExp(`^https://deno.land/std@0.150.0/media_types/mod.ts$`) }, (args) => {
-                    return { path: `https://deno.land/std@0.177.0/media_types/mod.ts`, external: true };
-                });
-            },
-        },
         ...denoPlugins({configPath: "/Users/josh/dev/ad4m/executor/deno.json"}), 
     ],
 });
