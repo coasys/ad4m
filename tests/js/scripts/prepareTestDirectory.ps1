cdd03ee6ce0503f4d7e02cb0f1602d7fd0d95e7b
Joshua Parkin <joshuadparkin@gmail.com>
2023-07-03 08:36:50 +0100

    remove swipl from prepareTestDirectory scripts

--- tests/js/scripts/prepareTestDirectory.ps1
+++ tests/js/scripts/prepareTestDirectory.ps1
@@ -11,5 +11,5 @@ wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/h
 
 wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/holochain-windows-0.1.0.exe -O ./holochain.exe
 
-$SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
-Copy-Item $SwiplPath -Destination swipl.exe
+# $SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
+# Copy-Item $SwiplPath -Destination swipl.exe
--- tests/js/scripts/prepareTestDirectory.sh
+++ tests/js/scripts/prepareTestDirectory.sh
@@ -9,9 +9,9 @@ mkdir note
 ln -s ../../../executor/temp/binary/hc ./hc
 ln -s ../../../executor/temp/binary/holochain ./holochain
 #ln -s ../../../executor/temp/swipl/bin/swipl ./swipl
-homedir=`echo "$(cd ../../../executor/temp/swipl/lib/swipl; pwd)"`
-echo '#!/bin/bash' > ./swipl
-echo 'cd ../../../executor/temp/swipl/bin' >> ./swipl
-echo -n "./swipl --home=${homedir} " >> ./swipl
-echo '"$@"' >> ./swipl
-chmod +x ./swipl
+#homedir=`echo "$(cd ../../../executor/temp/swipl/lib/swipl; pwd)"`
+# echo '#!/bin/bash' > ./swipl
+# echo 'cd ../../../executor/temp/swipl/bin' >> ./swipl
+# echo -n "./swipl --home=${homedir} " >> ./swipl
+# echo '"$@"' >> ./swipl
+# chmod +x ./swipl
