f810a2632b464c6d9aad0069279171167bc998a5
Joshua Parkin <joshuadparkin@gmail.com>
2023-07-05 13:05:50 +0100

    fix windows hc binary downloading

--- download-binaries-windows.ps1
+++ download-binaries-windows.ps1
@@ -1,6 +1,6 @@
 mkdir rust-executor/temp/binary
 
-wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/hc-windows-0.1.0.exe -O ./executor/temp/binary/hc.exe
+wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/hc-windows-0.1.0.exe -O ./rust-executor/temp/binary/hc.exe
 
 # Global path where the binary should be copied to
 $global_path = "C:\Windows\System32\"
