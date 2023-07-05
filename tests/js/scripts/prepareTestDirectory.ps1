d63d4df57e6a318e9d4b4f83b91303c7c4afa7ff
Joshua Parkin <joshuadparkin@gmail.com>
2023-07-05 18:06:59 +0100

    remove swipl copying from prepareTestDirectory script

--- tests/js/scripts/prepareTestDirectory.ps1
+++ tests/js/scripts/prepareTestDirectory.ps1
@@ -9,7 +9,7 @@ mkdir note
 
 wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/hc-windows-0.1.0.exe -O ./hc.exe
 
-wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/holochain-windows-0.1.0.exe -O ./holochain.exe
+# wget https://github.com/perspect3vism/ad4m/releases/download/binary-deps-0.1.0/holochain-windows-0.1.0.exe -O ./holochain.exe
 
-$SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
-Copy-Item $SwiplPath -Destination swipl.exe
+# $SwiplPath = Get-Command swipl.exe | Select-Object -ExpandProperty Definition
+# Copy-Item $SwiplPath -Destination swipl.exe
