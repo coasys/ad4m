aa6910b3a2cba39aa6da89b8d56326a52e420ce5
Nicolas Luck <nicolas@lucksus.eu>
2023-07-12 01:30:29 +0200

    Use Scryer fork with library use-case modifications

--- rust-executor/src/prolog.rs
+++ rust-executor/src/prolog.rs
@@ -1,46 +1,36 @@
-use std::io::Read;
-
 use scryer_prolog::machine::Machine;
 use scryer_prolog::machine::streams::*;
 
-fn string_2_stream(string: String, machine: &mut Machine) -> Stream {
-    let machine_st = machine.prelude_view_and_machine_st().1;
-    Stream::from_owned_string(string, &mut machine_st.arena)
-}
-
-fn print_output(machine: &mut Machine) {
-    let output_bytes: Vec<_> = machine.user_output.bytes().map(|b| b.unwrap()).collect();
-    let output_string = String::from_utf8(output_bytes).unwrap();
-    println!("{}", output_string);
-}
 
 pub fn run() {
     std::thread::spawn(|| {
         
         let mut machine = Machine::with_test_streams();
         
-        let facts = String::from("triple(\"a\", \"p1\", \"b\").");
-        //let facts = String::from("a.\n\n");
-        println!("Loading facts: {}", facts);
-        let fact_stream = string_2_stream(facts, &mut machine);
-        machine.load_file(String::from("facts.pl").as_str(), fact_stream);
-        print_output(&mut machine);
+        let facts = String::from(r#"
+triple("a", "p1", "b").
+triple("a", "p2", "b").
+"#);
+
+        machine.load_module_string("facts", facts);
+        println!("{}", machine.get_user_output());
         println!("Facts loaded");
         
         
+        
         //let query = String::from("triple(A,B,C), write(\"A = \"), write(A), nl, write(\"B = \"), write(B), write(\"C = \"), write(C), nl ; write(\"no triple matched\").\n");
-        let query = String::from("triple(A,B,C).\n");
+        let query = String::from("triple(\"a\",P,\"b\").");
         //let query = String::from("write(\"A = \").");
         //let query = String::from("halt.\n");
-        println!("Loading query: {}", query);
-        let query_stream = string_2_stream(query, &mut machine);
-        machine.user_input = query_stream;
+        println!("Running query: {}", query);
+        let output = machine.run_query(query);
+        println!("Output: {:?}", output);
 
-        println!("run once");
-        machine.run_once();
-        println!("run once done");
-
-        print_output(&mut machine);
-        println!("print output done");
+        let query = String::from("triple(\"a\",\"p1\",\"b\").");
+        //let query = String::from("write(\"A = \").");
+        //let query = String::from("halt.\n");
+        println!("Running query: {}", query);
+        let output = machine.run_query(query);
+        println!("Output: {:?}", output);
     });
 }   
