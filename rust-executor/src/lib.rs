9a1e9dc34763f12f5ff9138d82f10abe37394e34
Fayeed Pawaskar <fayeed@live.com>
2023-07-27 11:59:01 +0530

    Updated the rust-executor lib to work properly in the launcher

--- rust-executor/src/lib.rs
+++ rust-executor/src/lib.rs
@@ -5,12 +5,14 @@ mod holochain_service;
 mod js_core;
 mod utils;
 mod wallet;
+use tokio;
+
 
 pub mod init;
 mod pubsub;
 
-use log::{error, info};
 use std::env;
+use tracing::{info, error};
 
 //use graphql::start_server;
 use js_core::JsCore;
@@ -19,7 +21,7 @@ pub use config::Ad4mConfig;
 
 /// Runs the GraphQL server and the deno core runtime
 pub async fn run(mut config: Ad4mConfig) {
-    env::set_var("RUST_LOG", "rust_executor=trace,warp::server");
+    env::set_var("RUST_LOG", "rust_executor=info,warp::server");
     let _ = env_logger::try_init();
     config.prepare();
 
@@ -29,19 +31,23 @@ pub async fn run(mut config: Ad4mConfig) {
     info!("js_core initialized.");
 
     info!("Starting GraphQL...");
-    match graphql::start_server(
-        js_core_handle,
-        config.gql_port.expect("Did not get gql port"),
-    )
-    .await
-    {
-        Ok(_) => {
-            info!("GraphQL server stopped.");
-            std::process::exit(0);
-        }
-        Err(err) => {
-            error!("GraphQL server stopped with error: {}", err);
-            std::process::exit(1);
-        }
-    }
+
+    tokio::task::spawn_blocking(move || {
+        let result = graphql::start_server(
+            js_core_handle,
+            config.gql_port.expect("Did not get gql port"),
+        );
+        tokio::runtime::Handle::current().block_on(async {
+            match result.await {
+                Ok(_) => {
+                    info!("GraphQL server stopped.");
+                    std::process::exit(0);
+                }
+                Err(err) => {
+                    error!("GraphQL server stopped with error: {}", err);
+                    std::process::exit(1);
+                }
+            }
+        });
+    });
 }
