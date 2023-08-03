6f896d86bd988b85fd4b74e8cd0e370a3d58f79c
Nicolas Luck <nicolas@lucksus.eu>
2023-08-03 17:16:08 +0200

    Remove debug out

--- rust-executor/src/prolog_service/prolog_service_extension.js
+++ rust-executor/src/prolog_service/prolog_service_extension.js
@@ -10,7 +10,6 @@
             return JSON.parse(await core.opAsync("run_query", engineName, query));
         },
         loadModuleString: async (engineName, module_name, program_lines) => {
-            console.log("JS PROGRAM: "+program_lines)
             return core.opAsync("load_module_string", engineName, module_name, program_lines);
         }
     };
