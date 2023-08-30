dc9a073c46aef5fd979442d5c49aa2b6e6dccf80
Joshua Parkin <joshuadparkin@gmail.com>
2023-08-30 15:32:07 +0100

    use dirs package instead of depreciated env::home_dir

--- rust-executor/src/utils.rs
+++ rust-executor/src/utils.rs
@@ -1,6 +1,5 @@
-use std::env::home_dir;
-use std::path::{Path, PathBuf};
-
+use std::path::PathBuf;
+use dirs::home_dir;
 
 pub(crate) fn ad4m_data_directory() -> PathBuf {
     let ad4m_dir = home_dir().unwrap().join(".ad4m");
