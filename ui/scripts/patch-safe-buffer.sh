8ad78ecac86ee656a6010361c4eb8ec97b80cf32
Joshua Parkin <joshuadparkin@gmail.com>
2023-10-22 17:52:36 +0100

    refresh variables windows and try cargo clean before package build

--- ui/scripts/patch-safe-buffer.sh
+++ ui/scripts/patch-safe-buffer.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+
 sed "s/import buffer from 'node:buffer';/const buffer = require('buffer');/" ../node_modules/safe-buffer/index.js
 
 sed "s/import buffer from 'node:buffer'/const buffer = require('buffer');/" ../node_modules/safer-buffer/safer.js
