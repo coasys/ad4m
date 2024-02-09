78b9bf96c326456f7af81d80fcd180bfebca879d
Nicolas Luck <nicolas@lucksus.eu>
2024-02-09 15:54:16 +0100

    Merge rust_executor::run and run_with_tokio

--- rust-executor/src/lib.rs
+++ rust-executor/src/lib.rs
@@ -13,7 +13,7 @@ pub mod init;
 mod pubsub;
 mod dapp_server;
 
-use std::env;
+use std::{env, thread::JoinHandle};
 use log::info;
 
 use js_core::JsCore;
@@ -24,50 +24,7 @@ pub use holochain_service::run_local_hc_services;
 use crate::{prolog_service::init_prolog_service, dapp_server::serve_dapp};
 
 /// Runs the GraphQL server and the deno core runtime
-pub async fn run(mut config: Ad4mConfig) {
-    env::set_var("RUST_LOG", "holochain=warn,wasmer_compiler_cranelift=warn,rust_executor=info,warp::server");
-    let _ = env_logger::try_init();
-    config.prepare();
-
-    info!("Initializing Prolog service...");
-    init_prolog_service().await;
-
-    info!("Starting js_core...");
-    let mut js_core_handle = JsCore::start(config.clone()).await;
-    info!("Finished start");
-    js_core_handle.initialized().await;
-    info!("js_core initialized.");
-
-    info!("Starting GraphQL...");
-
-    if config.run_dapp_server.unwrap() {
-        std::thread::spawn(|| {
-            let runtime = tokio::runtime::Builder::new_multi_thread()
-                .enable_all()
-                .thread_name(String::from("dapp_server"))
-                .build()
-                .unwrap();
-            runtime.block_on(serve_dapp(8080)).unwrap();
-        });
-    }
-
-    let handle = std::thread::spawn(move || {
-        let runtime = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .thread_name(String::from("graphql_server"))
-            .build()
-            .unwrap();
-        runtime.block_on(graphql::start_server(
-            js_core_handle,
-            config.gql_port.expect("Did not get gql port"),
-            config.app_data_path.expect("Did not get app data path")
-        )).unwrap();
-    });
-    handle.join().unwrap();
-}
-
-/// Runs the GraphQL server and the deno core runtime
-pub async fn run_with_tokio(mut config: Ad4mConfig) {
+pub async fn run(mut config: Ad4mConfig) -> JoinHandle<()> {
     env::set_var("RUST_LOG", "holochain=warn,wasmer_compiler_cranelift=warn,rust_executor=debug,warp::server");
     let _ = env_logger::try_init();
     config.prepare();
@@ -104,7 +61,5 @@ pub async fn run_with_tokio(mut config: Ad4mConfig) {
             config.gql_port.expect("Did not get gql port"),
             config.app_data_path.expect("Did not get app data path")
         )).unwrap();
-    });
-
-    //TODO; we need someway to know that the graphql server is running before we allow this function to return
+    })
 }
