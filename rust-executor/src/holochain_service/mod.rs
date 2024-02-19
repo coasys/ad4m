1879f8926fa795907d376c9e2d0ac8e47ecb25a8
Nicolas Luck <nicolas@lucksus.eu>
2024-02-19 13:46:38 +0100

    Set prod bootstrap and signal server

--- rust-executor/src/holochain_service/mod.rs
+++ rust-executor/src/holochain_service/mod.rs
@@ -328,10 +328,11 @@ impl HolochainService {
             kitsune_config.tuning_params = Arc::new(tuning_params);
 
             if local_config.use_bootstrap {
+                // prod - https://bootstrap.holo.host
                 // staging - https://bootstrap-staging.holo.host
                 // dev - https://bootstrap-dev.holohost.workers.dev
                 // own - http://207.148.16.17:38245
-                kitsune_config.bootstrap_service = Some(Url2::parse(String::from("https://bootstrap-dev.holohost.workers.dev"))); 
+                kitsune_config.bootstrap_service = Some(Url2::parse(String::from("https://bootstrap.holo.host/"))); 
             } else {
                 kitsune_config.bootstrap_service = None;
             }
@@ -342,9 +343,10 @@ impl HolochainService {
             }
             if local_config.use_proxy {
                 kitsune_config.transport_pool = vec![TransportConfig::WebRTC {
+                    // prod - wss://signal.holo.host
                     // dev - wss://signal.holotest.net
                     // our - ws://207.148.16.17:42697
-                    signal_url: String::from("wss://signal.holotest.net"),
+                    signal_url: String::from("wss://signal.holo.host"),
                 }];
             } else {
                 kitsune_config.transport_pool = vec![
