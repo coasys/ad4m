f3fbbc93ff31657ced490be307db649935d62808
Nicolas Luck <nicolas@lucksus.eu>
2024-02-20 16:27:08 +0100

    Add signatures.rs

---
+++ rust-executor/src/agent/signatures.rs
@@ -0,0 +1,49 @@
+use serde_json::json;
+use sha2::{Sha256, Digest};
+use ad4m_client::types::Expression;
+use did_key::{CoreSign, PatchedKeyPair};
+use log::error;
+
+pub fn verify_string_signed_by_did(did: &str, _did_signing_key_id: &str, data: &str, signed_data: &str) -> bool {
+    let sig_bytes = hex::decode(signed_data).expect("Invalid hex in signed_data");
+    let message = build_message_raw(&serde_json::Value::String(data.to_owned()));
+    inner_verify(did, &message, &sig_bytes)
+}
+
+pub fn verify(expr: &Expression) -> bool {
+    let sig_bytes = hex::decode(&expr.proof.signature).expect("Invalid hex in expr.proof.signature");
+    let message = build_message(&expr.data, &expr.timestamp);
+    inner_verify(&expr.author, &message, &sig_bytes)
+}
+
+fn build_message(data: &serde_json::Value, timestamp: &str) -> Vec<u8> {
+    let payload  = json!({ "data": data, "timestamp": timestamp });
+    let payload_string = serde_json::to_string(&payload).expect("Failed to serialize payload");
+    let mut hasher = Sha256::new();
+    hasher.update(payload_string.as_bytes());
+    hasher.finalize().as_slice().try_into().expect("Hash should be 32 bytes")
+}
+
+fn build_message_raw(data: &serde_json::Value) -> Vec<u8> {
+    let payload = json!({ "data": data });
+    let payload_string = serde_json::to_string(&payload).expect("Failed to serialize payload");
+    let mut hasher = Sha256::new();
+    hasher.update(payload_string.as_bytes());
+    hasher.finalize().as_slice().try_into().expect("Hash should be 32 bytes")
+}
+
+
+fn inner_verify(did: &str, message: &[u8], signature: &[u8]) -> bool {
+    if let Ok(key_pair) = PatchedKeyPair::try_from(did) {
+        match key_pair.verify(message, signature) {
+            Ok(_) => true,
+            Err(e) => {
+                error!("Signature verification failed: {:?}", e);
+                false
+            }
+        }
+    } else {
+        error!("Failed to parse DID as key method: {}", did);
+        false
+    }
+}
