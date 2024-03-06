c90434753a0cb5a918dd75b3d6ab0035c56dd131
Nicolas Luck <nicolas@lucksus.eu>
2024-03-06 15:42:21 +0100

    Adjust enum PerspectiveState strings on JS side

--- core/src/perspectives/PerspectiveHandle.ts
+++ core/src/perspectives/PerspectiveHandle.ts
@@ -2,13 +2,12 @@ import { Field, ObjectType } from "type-graphql";
 import { NeighbourhoodExpression } from "../neighbourhood/Neighbourhood";
 
 export enum PerspectiveState {
-    Private = "Private",
-    NeighbourhoodJoinInitiated = "NeighbourhoodJoinInitiated",
-    LinkLanguageFailedToInstall = "LinkLanguageFailedToInstall",
-    LinkLanguageInstalledButNotSynced = "LinkLanguageInstalledButNotSynced",
-    Synced = "Synced",
+    Private = "PRIVATE",
+    NeighbourhoodJoinInitiated = "NEIGHBOURHOODJOININITIATED",
+    LinkLanguageFailedToInstall = "LINKLANGUAGEFAILEDTOINSTALL",
+    LinkLanguageInstalledButNotSynced = "LINKLANGUAGEINSTALLEDBUTNOTSYNCED",
+    Synced = "SYNCED",
 }
-
 // This type is used in the GraphQL interface to reference a mutable
 // prespective that is implemented locally by the Ad4m runtime.
 // The UUID is used in mutations to identify the perspective that gets mutated.
