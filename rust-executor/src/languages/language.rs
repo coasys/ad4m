b27d6dd00b84108816197a281e1473cd490f403a
Nicolas Luck <nicolas@lucksus.eu>
2024-03-12 22:09:45 +0100

    Extend Language struct with needed linksAdapter wrappings

--- rust-executor/src/languages/language.rs
+++ rust-executor/src/languages/language.rs
@@ -28,4 +28,56 @@ impl Language {
         let result = js.execute(script).await?;
         let result: JsResultType<Vec<String>> = serde_json::from_str(&result)?;
     }
+
+    pub async fn sync() -> Result<(), AnyError> {
+        let script = format!(
+            r#"
+            JSON.stringify(
+                await core
+                    .languageController
+                    .languageByRef("{}")
+                    .getLinksAdapter()
+                    .sync() 
+            )
+            "#,
+            address,
+        );
+        let _result: String = Self::global_instance().js_core.execute(script).await?;
+        Ok()
+    }
+
+    pub async fn commit(diff: PerspectiveDiff) -> Result<(), AnyError> {
+        let script = format!(
+            r#"
+            JSON.stringify(
+                await core
+                    .languageController
+                    .languageByRef("{}")
+                    .getLinksAdapter()
+                    .commit({}) 
+            )
+            "#,
+            address,
+            serde_json::to_string(&diff)?,
+        );
+        let _result: String = Self::global_instance().js_core.execute(script).await?;
+        Ok()
+    }
+
+    pub async fn currentRevision() -> Result<String, AnyError> {
+        let script = format!(
+            r#"
+            JSON.stringify(
+                await core
+                    .languageController
+                    .languageByRef("{}")
+                    .getLinksAdapter()
+                    .currentRevision() 
+            )
+            "#,
+            address,
+        );
+        let result: String = Self::global_instance().js_core.execute(script).await?;
+        Ok(result)
+    }
 }
