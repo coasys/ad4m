b4880c9a92f6322ed6d1812360f405d5f6dadcf5
Nicolas Luck <nicolas@lucksus.eu>
2024-03-16 21:59:52 +0100

    Fix language extension function names

--- rust-executor/src/js_core/languages_extension.js
+++ rust-executor/src/js_core/languages_extension.js
@@ -3,13 +3,13 @@
 
     globalThis.LANGUAGE_CONTROLLER = {
         perspectiveDiffReceived: (diff, language_address) => {
-            return core.ops.perspectiveDiffReceived(diff, language_address);
+            return core.ops.perspective_diff_received(diff, language_address);
         },
         syncStateChanged: (syncState, language_address) => {
-            return core.ops.syncStateChanged(syncState, language_address);
+            return core.ops.sync_state_changed(syncState, language_address);
         },
         telepresenceSignalReceived: (signal, language_address) => {
-            return core.ops.telepresenceSignalReceived(signal, language_address);
+            return core.ops.telepresence_signal_received(signal, language_address);
         },
     };
   })(globalThis);
