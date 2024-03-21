548c1ffd59d6ee39125a0ea05bb9cd186a5266cb
Nicolas Luck <nicolas@lucksus.eu>
2024-03-21 21:18:17 +0100

    Debug build.rs

--- rust-executor/build.rs
+++ rust-executor/build.rs
@@ -16,11 +16,17 @@ fn copy_dir_recursive(source: &Path, target: &Path) -> std::io::Result<()> {
 }
 
 fn main() {
-    let source_dir = "../dapp/public";
+    println!("cargo:rerun-if-changed=build.rs"); // Ensure the build script is re-run if it is changed
+    println!("cargo:rerun-if-changed=../dapp/public"); // Re-run if the dapp directory changes
 
+    let source_dir = "../dapp/public";
     let target_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/dapp");
+
+    println!("Copying from {} to {}", source_dir, target_dir);
+
 
     if let Err(err) = copy_dir_recursive(Path::new(source_dir), Path::new(target_dir)) {
         eprintln!("Error copying directory: {}", err);
+        std::process::exit(1); // Exit with an error code to fail the build if the copy fails
     }
 }
