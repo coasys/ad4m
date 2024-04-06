5a16f1144cbb87c700eeaa1772e8d88a5866a91e
Fayeed Pawaskar <fayeed@live.com>
2024-04-06 08:28:40 +0530

    Refactor RuntimeService to use a global instance

--- rust-executor/src/runtime_service/mod.rs
+++ rust-executor/src/runtime_service/mod.rs
@@ -1,6 +1,7 @@
 use std::{ collections::HashMap, env, fs::{self, File}, io, path::Path, sync::Mutex};
 use std::io::{Read};
 pub(crate) mod runtime_service_extension;
+use std::sync::Arc;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BootstrapSeed {
@@ -25,101 +26,123 @@ use serde::{Deserialize, Serialize};
 use crate::{agent::did, db::Ad4mDb, graphql::graphql_types::SentMessage};
 
 lazy_static! {
-    static ref MAINNETSEED_FILE: Mutex<String> = Mutex::new(format!("{}/mainnet_seed.seed", env::var("APPS_DATA_PATH").unwrap_or_else(|_| "".to_string())));
+    static ref RUNTIME_INSTANCE: Arc<Mutex<Option<RuntimeService>>> = Arc::new(Mutex::new(None));
 }
 
-fn get_mainnet_seed_file_path() -> String {
-    MAINNETSEED_FILE.lock().unwrap().clone()
+pub struct RuntimeService {
+    seed: BootstrapSeed,
 }
 
-fn load_mainnet_seed_from_file() -> io::Result<BootstrapSeed> {
-    let file_path = get_mainnet_seed_file_path();
-    let mut file = File::open(&file_path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let seed: BootstrapSeed = serde_json::from_str(&contents)?;
-    Ok(seed)
-}
+impl RuntimeService {
+    pub fn init_global_instance(mainnet_seed_file_path: String) {
+        let mut instance = RUNTIME_INSTANCE.lock().unwrap();
+        *instance = Some(RuntimeService::new(mainnet_seed_file_path));
+    }
 
-pub fn get_trusted_agents() -> Vec<String> {
-    let seed = load_mainnet_seed_from_file().unwrap();
-    let mut trusted_agents: Vec<String> = seed.trusted_agents.clone();
-    let mut stored_agents = Ad4mDb::with_global_instance(|db| {
-        db.get_all_trusted_agents()
-    }).map_err(|e| e.to_string()).unwrap_or(vec![]);
-    trusted_agents.push(did());
-    trusted_agents.append(&mut stored_agents);
-    trusted_agents.sort();
-    trusted_agents.dedup();
-    trusted_agents
-}
+    fn new(mainnet_seed_file_path: String) -> RuntimeService {
+        let mut file = File::open(&mainnet_seed_file_path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let seed: BootstrapSeed = serde_json::from_str(&contents).unwrap();
 
-pub fn add_trusted_agent(new_agents: Vec<String>) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.add_trusted_agents(new_agents)
-    }).map_err(|e| e.to_string());
-}
+        RuntimeService {
+            seed
+        }
+    }
 
-pub fn remove_trusted_agent(agents_to_remove: Vec<String>) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.remove_trusted_agents(agents_to_remove)
-    }).map_err(|e| e.to_string());
-}
+    pub fn global_instance() -> Arc<Mutex<Option<RuntimeService>>> {
+        RUNTIME_INSTANCE.clone()
+    }
 
-pub fn get_know_link_languages() -> Vec<String> {
-    let seed = load_mainnet_seed_from_file().unwrap();
-    let mut languages: Vec<String> = seed.known_link_languages.clone();
-    let mut stored_languages = Ad4mDb::with_global_instance(|db| {
-        db.get_all_known_link_languages()
-    }).map_err(|e| e.to_string()).unwrap_or(vec![]);
-    languages.append(&mut stored_languages);
-    languages.sort();
-    languages.dedup();
-    languages
-}
+    pub fn with_global_instance<F, R>(func: F) -> R
+    where
+        F: FnOnce(&RuntimeService) -> R,
+    {
+        let global_instance_arc = RuntimeService::global_instance();
+        let lock_result = global_instance_arc.lock();
+        let runtime_lock = lock_result.expect("Couldn't get lock on Ad4mDb");
+        let runtime_ref = runtime_lock.as_ref().expect("Ad4mDb not initialized");
+        func(runtime_ref)
+    }
 
-pub fn add_know_link_language(language: Vec<String>) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.add_known_link_languages(language)
-    }).map_err(|e| e.to_string());
-}
+    pub fn get_trusted_agents(&self) -> Vec<String> {
+        let mut trusted_agents: Vec<String> = self.seed.trusted_agents.clone();
+        let mut stored_agents = Ad4mDb::with_global_instance(|db| {
+            db.get_all_trusted_agents()
+        }).map_err(|e| e.to_string()).unwrap_or(vec![]);
+        trusted_agents.push(did());
+        trusted_agents.append(&mut stored_agents);
+        trusted_agents.sort();
+        trusted_agents.dedup();
+        trusted_agents
+    }
 
-pub fn remove_know_link_language(language_to_remove: Vec<String>) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.remove_known_link_languages(language_to_remove)
-    }).map_err(|e| e.to_string());
-}
+    pub fn add_trusted_agent(&self, new_agents: Vec<String>) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.add_trusted_agents(new_agents)
+        }).map_err(|e| e.to_string());
+    }
 
-pub fn get_friends() -> Vec<String> {
-    let friends = Ad4mDb::with_global_instance(|db| {
-        db.get_all_friends()
-    }).map_err(|e| e.to_string()).unwrap_or(vec![]);
+    pub fn remove_trusted_agent(&self, agents_to_remove: Vec<String>) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.remove_trusted_agents(agents_to_remove)
+        }).map_err(|e| e.to_string());
+    }
 
-    friends
-}
+    pub fn get_know_link_languages(&self) -> Vec<String> {
+        let mut languages: Vec<String> = self.seed.known_link_languages.clone();
+        let mut stored_languages = Ad4mDb::with_global_instance(|db| {
+            db.get_all_known_link_languages()
+        }).map_err(|e| e.to_string()).unwrap_or(vec![]);
+        languages.append(&mut stored_languages);
+        languages.sort();
+        languages.dedup();
+        languages
+    }
 
-pub fn add_friend(friends: Vec<String>) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.add_friends(friends)
-    }).map_err(|e| e.to_string());
-}
+    pub fn add_know_link_language(&self, language: Vec<String>) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.add_known_link_languages(language)
+        }).map_err(|e| e.to_string());
+    }
 
-pub fn remove_friend(friend_to_remove: Vec<String>) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.remove_friends(friend_to_remove)
-    }).map_err(|e| e.to_string());
-}
+    pub fn remove_know_link_language(&self, language_to_remove: Vec<String>) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.remove_known_link_languages(language_to_remove)
+        }).map_err(|e| e.to_string());
+    }
 
-pub fn get_outbox() -> Vec<SentMessage> {
-    let outbox = Ad4mDb::with_global_instance(|db| {
-        db.get_all_from_outbox()
-    }).map_err(|e| e.to_string()).unwrap_or(vec![]);
+    pub fn get_friends(&self) -> Vec<String> {
+        let friends = Ad4mDb::with_global_instance(|db| {
+            db.get_all_friends()
+        }).map_err(|e| e.to_string()).unwrap_or(vec![]);
 
-    outbox
-}
+        friends
+    }
 
-pub fn add_message_to_outbox(message: SentMessage) {
-    let _ = Ad4mDb::with_global_instance(|db| {
-        db.add_to_outbox(&message.message, message.recipient)
-    }).map_err(|e| e.to_string());
+    pub fn add_friend(&self, friends: Vec<String>) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.add_friends(friends)
+        }).map_err(|e| e.to_string());
+    }
+
+    pub fn remove_friend(&self, friend_to_remove: Vec<String>) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.remove_friends(friend_to_remove)
+        }).map_err(|e| e.to_string());
+    }
+
+    pub fn get_outbox(&self) -> Vec<SentMessage> {
+        let outbox = Ad4mDb::with_global_instance(|db| {
+            db.get_all_from_outbox()
+        }).map_err(|e| e.to_string()).unwrap_or(vec![]);
+
+        outbox
+    }
+
+    pub fn add_message_to_outbox(&self, message: SentMessage) {
+        let _ = Ad4mDb::with_global_instance(|db| {
+            db.add_to_outbox(&message.message, message.recipient)
+        }).map_err(|e| e.to_string());
+    }
 }
