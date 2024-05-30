5602bb62be5b6e5fed12851cefe7e5ca83ff1700
Fayeed Pawaskar <fayeed@live.com>
2024-05-30 11:14:20 +0530

    chore: Update Notification component to fetch and display perspectives

--- ui/src/components/Notification.tsx
+++ ui/src/components/Notification.tsx
@@ -1,19 +1,28 @@
 import { useContext, useEffect, useState } from "react";
 import { Ad4minContext } from "../context/Ad4minContext"
-import { Notification as NotificationType } from "@coasys/ad4m/lib/src/runtime/RuntimeResolver";;
+import { Notification as NotificationType } from "@coasys/ad4m/lib/src/runtime/RuntimeResolver"; import { PerspectiveProxy } from "@coasys/ad4m";
+;
 
-const Notification = ({notification}: {notification: NotificationType}) => {
+const Notification = ({ notification }: { notification: NotificationType }) => {
   const {
-    state: { client  },
+    state: { client },
     methods: { handleNotification },
   } = useContext(Ad4minContext);
 
   const [requestModalOpened, setRequestModalOpened] = useState(true);
+  const [perspectives, setPerspectives] = useState<PerspectiveProxy[]>([]);
 
   useEffect(() => {
     setRequestModalOpened(true);
+    getPerspectives();
   }, [notification]);
 
+
+  const getPerspectives = async () => {
+    const perspectives = await client?.perspective.all();
+    const filteredPerspectives = perspectives?.filter((perspective) => notification.perspectiveIds.includes(perspective.uuid));
+    setPerspectives(filteredPerspectives);
+  }
 
   const permitNotification = async () => {
     // @ts-ignore
@@ -56,6 +65,42 @@ const Notification = ({notification}: {notification: NotificationType}) => {
                   <j-text nomargin size="500">
                     {notification?.description}
                   </j-text>
+                </div>
+              </j-flex>
+              <j-flex gap="200" direction="column">
+                <div>
+                  <j-text nomargin size="500">
+                    Perspectives:
+                  </j-text>
+                  <ul>
+                    {perspectives?.map((perspective) => (
+                      <li key={perspective.uuid}>
+                        <j-text nomargin size="500">
+                          {perspective.name}
+                        </j-text>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <j-flex>
+                    <j-text>
+                      Notification Trigger: {notification?.trigger}
+                    </j-text>
+                  </j-flex>
+                </div>
+                <div>
+                  <j-flex>
+                    <j-text>
+                      Webhook URL: {notification?.webhookUrl}
+                    </j-text>
+                  </j-flex>
+                  <j-text color="danger" variant="caption">
+
+                  </j-text>
+                  <p style="height: 60px; color: red; font-size: 14px; margin: 0; ">
+                    Caution: This notification will be sent to the above URL and the data can be leaked outside of the app. Please make sure you trust the app.
+                  </p>
                 </div>
               </j-flex>
 
