b7d6c748b9123ed154e3d7d55dea451501b5444a
Fayeed Pawaskar <fayeed@live.com>
2024-06-08 11:09:18 +0530

    chore: Update dependencies and extensions for rust-executor

--- rust-executor/src/js_core/wallet_extension.js
+++ rust-executor/src/js_core/wallet_extension.js
@@ -1,33 +1,37 @@
-((globalThis) => {
-    const core = Deno.core;
+import {
+    wallet_create_main_key, wallet_get_main_key, wallet_get_main_key_document, 
+    wallet_is_unlocked, wallet_unlock, wallet_lock, wallet_export, wallet_load, 
+    wallet_sign
+} from 'ext:core/ops'
 
+((globalThis) => {
     globalThis.WALLET = {
         getMainKey: () => {
-            return core.ops.wallet_get_main_key();
+            return wallet_get_main_key();
         },
         getMainKeyDocument: () => {
-            return core.ops.wallet_get_main_key_document();
+            return wallet_get_main_key_document();
         },
         createMainKey: () => {
-            return core.ops.wallet_create_main_key();
+            return wallet_create_main_key();
         },
         isUnlocked: () => {
-            return core.ops.wallet_is_unlocked();
+            return wallet_is_unlocked();
         },
         unlock: (password) => {
-            return core.ops.wallet_unlock(password);
+            return wallet_unlock(password);
         },
         lock: (password) => {
-            return core.ops.wallet_lock(password);
+            return wallet_lock(password);
         },
         export: (password) => {
-            return core.ops.wallet_export(password);
+            return wallet_export(password);
         },
         load: (data) => {
-            return core.ops.wallet_load(data);
+            return wallet_load(data);
         },
         sign: (payload) => {
-            return core.ops.wallet_sign(payload);
+            return wallet_sign(payload);
         },
     };
   })(globalThis);
--- rust-executor/src/prolog_service/prolog_service_extension.js
+++ rust-executor/src/prolog_service/prolog_service_extension.js
@@ -1,19 +1,21 @@
-((globalThis) => {
-    const core = Deno.core;
+import {
+    spawn_engine, remove_engine, run_query, load_module_string
+} from 'ext:core/ops'
 
+((globalThis) => {
     globalThis.PROLOG_SERVICE = {
         spawnEngine: async (engineName) => {
-            return core.opAsync("spawn_engine", engineName);
+            return spawn_engine(engineName);
         },
         removeEngine: async (engineName) => {
-            return core.opAsync("remove_engine", engineName);
+            return remove_engine(engineName);
         },
         runQuery: async (engineName, query) => {
             if(!query.endsWith(".")) query = query+".";
-            return JSON.parse(await core.opAsync("run_query", engineName, query));
+            return JSON.parse(await run_query(engineName, query));
         },
         loadModuleString: async (engineName, module_name, program_lines) => {
-            return core.opAsync("load_module_string", engineName, module_name, program_lines);
+            return load_module_string(engineName, module_name, program_lines);
         }
     };
   })(globalThis);
