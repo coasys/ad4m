5e62eec2e37345e48452dc8a4b486c23cb683670
Nicolas Luck <nicolas@lucksus.eu>
2024-07-04 17:04:42 +0200

    Process expression.get() on client for literals (no signature checking)

--- core/src/expression/ExpressionClient.ts
+++ core/src/expression/ExpressionClient.ts
@@ -2,6 +2,7 @@ import { ApolloClient, gql } from "@apollo/client/core";
 import { InteractionCall, InteractionMeta } from "../language/Language";
 import unwrapApolloResult from "../unwrapApolloResult";
 import { ExpressionRendered } from "./Expression";
+import { Literal } from "../Literal";
 
 export class ExpressionClient {
     #apolloClient: ApolloClient<any>
@@ -11,6 +12,15 @@ export class ExpressionClient {
     }
 
     async get(url: string): Promise<ExpressionRendered> {
+        try {
+            let literalValue = Literal.fromUrl(url).get();
+            if (typeof literalValue === 'object' && literalValue !== null) {
+                if ('author' in literalValue && 'timestamp' in literalValue && 'data' in literalValue && 'proof' in literalValue) {
+                    return literalValue;
+                }
+            }
+        } catch(e) {}
+
         const { expression } = unwrapApolloResult(await this.#apolloClient.query({
             query: gql`query expression($url: String!) {
                 expression(url: $url) {
