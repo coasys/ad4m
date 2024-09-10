1b65c1c9b7e33807490517c039e3e6415a8467a2
Nicolas Luck <nicolas@lucksus.eu>
2024-09-10 17:35:09 +0200

    Move tasks into Llama thread, LLMTaskRequest structs, more fixes, tests fixed

--- rust-executor/src/ai_service/mod.rs
+++ rust-executor/src/ai_service/mod.rs
@@ -6,10 +6,10 @@ use anyhow::anyhow;
 use deno_core::error::AnyError;
 use kalosm::language::*;
 use std::collections::HashMap;
-use std::sync::{Arc, mpsc};
-use std::thread;
-use tokio::sync::{Mutex, oneshot};
 use std::panic::catch_unwind;
+use std::sync::Arc;
+use std::thread;
+use tokio::sync::{mpsc, oneshot, Mutex};
 
 mod error;
 
@@ -21,52 +21,170 @@ lazy_static! {
 
 #[derive(Clone)]
 pub struct AIService {
-    bert: mpsc::Sender<(String, oneshot::Sender<Result<String>>)>,
-    llama: mpsc::Sender<(String, oneshot::Sender<Result<String>>)>,
-    tasks: Arc<Mutex<HashMap<String, Task>>>,
+    bert: mpsc::UnboundedSender<EmbeddingRequest>,
+    llama: mpsc::UnboundedSender<LLMTaskRequest>,
+}
+
+struct EmbeddingRequest {
+    pub prompt: String,
+    pub result_sender: oneshot::Sender<Result<Vec<f32>>>,
+}
+
+#[derive(Debug)]
+struct LLMTaskSpawnRequest {
+    pub task: AITask,
+    pub result_sender: oneshot::Sender<Result<()>>,
+}
+
+#[derive(Debug)]
+struct LLMTaskPromptRequest {
+    pub task_id: String,
+    pub prompt: String,
+    pub result_sender: oneshot::Sender<Result<String>>,
+}
+
+#[derive(Debug)]
+struct LLMTaskRemoveRequest {
+    pub task_id: String,
+    pub result_sender: oneshot::Sender<()>,
+}
+
+#[derive(Debug)]
+enum LLMTaskRequest {
+    Spawn(LLMTaskSpawnRequest),
+    Prompt(LLMTaskPromptRequest),
+    Remove(LLMTaskRemoveRequest),
 }
 
 impl AIService {
     pub async fn new() -> Result<Self> {
-        let (bert_tx, bert_rx) = mpsc::channel();
-        let (llama_tx, llama_rx) = mpsc::channel();
+        let (bert_tx, mut bert_rx) = mpsc::unbounded_channel::<EmbeddingRequest>();
+        let (llama_tx, mut llama_rx) = mpsc::unbounded_channel::<LLMTaskRequest>();
 
         thread::spawn(move || {
             let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(async {
-                let bert = Bert::builder().build().await.unwrap();
-                for (prompt, response_tx) in bert_rx {
-                    let result = catch_unwind(|| rt.block_on(bert.embed(prompt))).map_err(|_| "Panic occurred".into());
-                    let _ = response_tx.send(result);
-                }
-            });
+            let bert = rt
+                .block_on(Bert::builder().build())
+                .expect("couldn't build Bert model");
+            while let Some(request) = rt.block_on(bert_rx.recv()) {
+                // let result: Result<Vec<f32>> = match catch_unwind(|| rt.block_on(bert.embed(request.prompt))) {
+                //     Err(e) => Err(anyhow!("Bert panicked: {:?}", e)),
+                //     Ok(embed_result) => embed_result.and_then(|tensor| Ok(tensor.to_vec()))
+                // };
+
+                let result: Result<Vec<f32>> = rt
+                    .block_on(bert.embed(request.prompt))
+                    .and_then(|tensor| Ok(tensor.to_vec()));
+                let _ = request.result_sender.send(result);
+            }
         });
 
         thread::spawn(move || {
             let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(async {
-                let llama = Llama::builder()
-                    .with_source(LlamaSource::tiny_llama_1_1b())
-                    .build()
-                    .await
-                    .unwrap();
-                for (prompt, response_tx) in llama_rx {
-                    let result = catch_unwind(|| rt.block_on(llama.run(prompt))).map_err(|_| "Panic occurred".into());
-                    let _ = response_tx.send(result);
+            let llama = rt
+                .block_on(
+                    Llama::builder()
+                        .with_source(LlamaSource::tiny_llama_1_1b())
+                        .build(),
+                )
+                .expect("couldn't build Llama model");
+
+            let mut tasks = HashMap::<String, Task>::new();
+
+            while let Some(task_request) = rt.block_on(llama_rx.recv()) {
+                println!("Llama message received: {:?}", task_request);
+                match task_request {
+                    LLMTaskRequest::Spawn(spawn_request) => {
+                        let task_description = spawn_request.task;
+                        let task = Task::builder(task_description.system_prompt.clone())
+                            .with_examples(
+                                task_description
+                                    .prompt_examples
+                                    .clone()
+                                    .into_iter()
+                                    .map(|example| (example.input, example.output))
+                                    .collect::<Vec<(String, String)>>(),
+                            )
+                            .build();
+
+                        let mut task_run = false;
+                        let mut tries = 0;
+                        while !task_run && tries < 20 {
+                            tries += 1;
+                            match catch_unwind(|| {
+                                rt.block_on(task.run("Test example prompt", &llama).all_text())
+                            }) {
+                                Err(e) => log::error!(
+                                    "Llama panicked during task spawn with: {:?}. Trying again..",
+                                    e
+                                ),
+                                Ok(_) => task_run = true,
+                            }
+                        }
+
+                        if task_run {
+                            tasks.insert(task_description.task_id.clone(), task);
+                            let _ = spawn_request.result_sender.send(Ok(()));
+                        } else {
+                            let _ = spawn_request
+                                .result_sender
+                                .send(Err(anyhow!("Couldn't run task without panicks")));
+                        }
+                    }
+
+                    LLMTaskRequest::Prompt(prompt_request) => {
+                        println!("Got prompt request {:?}", prompt_request);
+                        if let Some(task) = tasks.get(&prompt_request.task_id) {
+                            let mut maybe_result: Option<String> = None;
+                            let mut tries = 0;
+                            while maybe_result.is_none() && tries < 20 {
+                                tries += 1;
+                                println!("Try: {}", tries);
+                                match catch_unwind(|| {
+                                    rt.block_on(
+                                        task.run(prompt_request.prompt.clone(), &llama).all_text(),
+                                    )
+                                }) {
+                                    Err(e) => {
+                                        log::error!("Llama panicked with: {:?}. Trying again..", e)
+                                    }
+                                    Ok(result) => maybe_result = Some(result),
+                                }
+                            }
+
+                            println!("maybe_result: {:?}", maybe_result);
+                            if let Some(result) = maybe_result {
+                                let _ = prompt_request.result_sender.send(Ok(result));
+                            } else {
+                                let _ = prompt_request.result_sender.send(Err(anyhow!("Unable to get response from Llama model. Giving up after 20 retries")));
+                            }
+                        } else {
+                            let _ = prompt_request.result_sender.send(Err(anyhow!(
+                                "Task with ID {} not spawned",
+                                prompt_request.task_id
+                            )));
+                        }
+                    }
+
+                    LLMTaskRequest::Remove(remove_request) => {
+                        let _ = tasks.remove(&remove_request.task_id);
+                        let _ = remove_request.result_sender.send(());
+                    }
                 }
-            });
+            }
+
+            println!("Llama thread exiting");
         });
 
         let service = AIService {
             bert: bert_tx,
             llama: llama_tx,
-            tasks: Arc::new(Mutex::new(HashMap::new())),
         };
 
         let tasks = Ad4mDb::with_global_instance(|db| db.get_tasks())
             .map_err(|e| AIServiceError::DatabaseError(e.to_string()))?;
 
-        for task in &tasks {
+        for task in tasks {
             service.spawn_task(task).await?;
         }
 
@@ -88,7 +206,7 @@ impl AIService {
             .ok_or(anyhow!(AIServiceError::ServiceNotInitialized))
     }
 
-    pub async fn add_task(&mut self, task: AITaskInput) -> Result<AITask> {
+    pub async fn add_task(&self, task: AITaskInput) -> Result<AITask> {
         let task_id = Ad4mDb::with_global_instance(|db| {
             db.add_task(
                 task.model_id.clone(),
@@ -104,15 +222,15 @@ impl AIService {
         let task = Ad4mDb::with_global_instance(|db| db.get_task(task_id))?
             .expect("to get task that we just created");
 
-        self.spawn_task(&task).await?;
+        self.spawn_task(task.clone()).await?;
         Ok(task)
     }
 
-    pub async fn delete_task(&mut self, task_id: String) -> Result<bool> {
+    pub async fn delete_task(&self, task_id: String) -> Result<bool> {
         Ad4mDb::with_global_instance(|db| db.remove_task(task_id.clone()))
             .map_err(|e| AIServiceError::DatabaseError(e.to_string()))?;
 
-        self.tasks.lock().await.remove(&task_id);
+        self.remove_task(task_id).await?;
 
         Ok(true)
     }
@@ -123,7 +241,7 @@ impl AIService {
         Ok(tasks)
     }
 
-    pub async fn update_task(&mut self, task: AITask) -> Result<AITask> {
+    pub async fn update_task(&self, task: AITask) -> Result<AITask> {
         let task_id = task.task_id.clone();
         Ad4mDb::with_global_instance(|db| {
             db.update_task(
@@ -135,61 +253,60 @@ impl AIService {
         })
         .map_err(|e| AIServiceError::DatabaseError(e.to_string()))?;
 
-        self.tasks.lock().await.remove(&task_id);
+        self.remove_task(task_id.clone()).await?;
 
         let updated_task = Ad4mDb::with_global_instance(|db| db.get_task(task_id.clone()))
             .map_err(|e| AIServiceError::DatabaseError(e.to_string()))?
             .ok_or(AIServiceError::TaskNotFound)?;
 
-        self.spawn_task(&updated_task).await?;
+        self.spawn_task(updated_task.clone()).await?;
 
         Ok(updated_task)
     }
 
     pub async fn prompt(&self, task_id: String, prompt: String) -> Result<String> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.llama.send((prompt, response_tx)).unwrap();
-        response_rx.await.unwrap_or_else(|_| Err("Failed to receive response".into()))
+        let (result_sender, rx) = oneshot::channel();
+        self.llama
+            .send(LLMTaskRequest::Prompt(LLMTaskPromptRequest {
+                task_id,
+                prompt,
+                result_sender,
+            }))?;
+        rx.await?
     }
 
     pub async fn embed(text: String) -> Result<Vec<f32>> {
-        Ok(AIService::global_instance()
+        let (result_sender, rx) = oneshot::channel();
+
+        AIService::global_instance()
             .await?
             .bert
-            .lock()
-            .await
-            .embed(text)
-            .await?
-            .to_vec())
+            .send(EmbeddingRequest {
+                prompt: text,
+                result_sender,
+            })?;
+
+        rx.await?
     }
 
-    async fn spawn_task(&self, task_description: &AITask) -> Result<()> {
-        let task = Task::builder(task_description.system_prompt.clone())
-            .with_examples(
-                task_description
-                    .prompt_examples
-                    .clone()
-                    .into_iter()
-                    .map(|example| (example.input, example.output))
-                    .collect::<Vec<(String, String)>>(),
-            )
-            .build();
+    async fn spawn_task(&self, task: AITask) -> Result<()> {
+        let (tx, rx) = oneshot::channel();
+        self.llama.send(LLMTaskRequest::Spawn(LLMTaskSpawnRequest {
+            task,
+            result_sender: tx,
+        }))?;
+        rx.await?
+    }
 
-        let _ = self
-            .run_task(&task, "Test example prompt".to_string())
-            .await;
-
-        self.tasks
-            .lock()
-            .await
-            .insert(task_description.task_id.clone(), task);
-
+    async fn remove_task(&self, task_id: String) -> Result<()> {
+        let (tx, rx) = oneshot::channel();
+        self.llama
+            .send(LLMTaskRequest::Remove(LLMTaskRemoveRequest {
+                task_id,
+                result_sender: tx,
+            }))?;
+        let _ = rx.await?;
         Ok(())
-    }
-
-    async fn run_task(&self, task: &Task, prompt: String) -> String {
-        let llama = self.llama.lock().await;
-        task.run(prompt, &*llama).all_text().await
     }
 }
 
@@ -201,6 +318,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_embedding() {
+        Ad4mDb::init_global_instance(":memory:").expect("Ad4mDb to initialize");
         AIService::init_global_instance()
             .await
             .expect("initialization to work");
@@ -213,22 +331,19 @@ mod tests {
     #[tokio::test]
     async fn test_prompt() {
         Ad4mDb::init_global_instance(":memory:").expect("Ad4mDb to initialize");
-        AIService::init_global_instance()
-            .await
-            .expect("initialization to work");
+        let service = AIService::new().await.expect("initialization to work");
 
-        let task = AIService::with_mutable_global_instance(|service| {
-            service.add_task(AITaskInput {
+        let task = service.add_task(AITaskInput {
                 model_id: "Llama tiny 1b".into(),
                 system_prompt: "You are inside a test for tasks. Please make sure to create any non-zero length output".into(),
                 prompt_examples: vec![AIPromptExamplesInput{
                     input: "Test string".into(),
                     output: "Yes, I'm working!".into()
                 }],
-            })
         }).await.expect("add_task to work without error");
 
-        let response = AIService::prompt(task.task_id, "Test string".into())
+        let response = service
+            .prompt(task.task_id, "Test string".into())
             .await
             .expect("prompt to return a result");
         println!("Response: {}", response);
