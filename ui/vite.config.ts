387a634de974bdbedaac436ee71baa71b9f869f7
jhweir <james@weco.io>
2024-11-04 20:52:03 +0000

    Externalise tauri-apps in vite rollupOptions

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
   build: {
     target: ["safari11"],
+    rollupOptions: {
+      external: (id) => id.startsWith("@tauri-apps/"),
+    },
   },
   server: {
     port: 3000,
