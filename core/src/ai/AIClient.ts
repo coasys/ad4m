19f6128ac6880ced6ef1411c76e0a3cb232413d0
Nicolas Luck <nicolas@lucksus.eu>
2024-11-29 13:59:41 +0100

    Add id field to qraphql query

--- core/src/ai/AIClient.ts
+++ core/src/ai/AIClient.ts
@@ -18,6 +18,7 @@ export class AIClient {
             query: gql`
                 query {
                     aiGetModels {
+                        id
                         name
                         api {
                             baseUrl
