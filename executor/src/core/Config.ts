dd055cefb5567200c4c15e3e3ba4b3ac226a17b4
Nicolas Luck <nicolas@lucksus.eu>
2025-01-29 23:04:28 +0100

    set version 0.10.1-rc7

--- executor/src/core/Config.ts
+++ executor/src/core/Config.ts
@@ -2,7 +2,7 @@ import * as path from 'node:path';
 import * as fs from 'node:fs';
 import { Address, Expression } from '@coasys/ad4m';
 
-export let ad4mExecutorVersion = "0.10.1-rc6";
+export let ad4mExecutorVersion = "0.10.1-rc7";
 export let agentLanguageAlias = "did";
 export let languageLanguageAlias = "lang";
 export let neighbourhoodLanguageAlias = "neighbourhood";
