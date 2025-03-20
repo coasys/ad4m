4ca72378df869f1c72201adebce4e3b490b2d21f
HexaField <HexaField@users.noreply.github.com>
2025-03-20 17:11:00 +1100

    add test for multiple neighbourhood apollo subscriptions

--- core/src/neighbourhood/NeighbourhoodProxy.test.ts
+++ core/src/neighbourhood/NeighbourhoodProxy.test.ts
@@ -1,5 +1,3 @@
-import { ApolloClient } from "@apollo/client";
-import { LinkExpression } from "../links/Links";
 import { NeighbourhoodClient } from "./NeighbourhoodClient";
 import { NeighbourhoodProxy } from "./NeighbourhoodProxy";
 
@@ -41,5 +39,62 @@ describe("NeighbourhoodProxy", () => {
     neighbourhoodClient.dispatchSignal(neighbourhoodURI, true);
 
     expect(callbacks).toBe(2);
+  });
+
+  it("should not add multiple subscriptions when removing and adding another signal handler", async () => {
+    const neighbourhoodURI = "did://123";
+
+    const subscriptions = [];
+    const subscribe = (args: { next: (result: any) => void }) => {
+      subscriptions.push(args);
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({});
+        }, 10);
+      });
+    };
+
+    const mockApolloClient = {
+      subscribe: () => ({
+        subscribe,
+      }),
+    } as any;
+
+    const neighbourhoodClient = new NeighbourhoodClient(mockApolloClient);
+    const neighbourhoodProxy = new NeighbourhoodProxy(
+      neighbourhoodClient,
+      neighbourhoodURI
+    );
+
+    let callbacks1 = 0;
+    let callbacks2 = 0;
+
+    const handler1 = () => {
+      callbacks1++;
+    };
+    const handler2 = () => {
+      callbacks2++;
+    };
+
+    // Add signal handler 1
+    await neighbourhoodProxy.addSignalHandler(handler1);
+
+    // Remove signal handler 2
+    neighbourhoodProxy.removeSignalHandler(handler1);
+
+    // Add signal handler 2
+    await neighbourhoodProxy.addSignalHandler(handler2);
+
+    // Check that only one subscription was added
+    expect(subscriptions.length).toBe(1);
+
+    // mock trigger signal event
+    for (const subscription of subscriptions) {
+      subscription.next(true);
+    }
+
+    // Check that only our second callback was called
+    expect(callbacks1).toBe(0);
+    expect(callbacks2).toBe(1);
   });
 });
