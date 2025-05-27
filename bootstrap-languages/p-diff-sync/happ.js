cd9af980323840cbc4a9510dc0214ad09e95acdb
Nicolas Luck <nicolas@lucksus.eu>
2025-05-27 20:19:20 +0200

    Fix import of happ file in p-diff-sync

--- bootstrap-languages/p-diff-sync/happ.js
+++ bootstrap-languages/p-diff-sync/happ.js
@@ -1,5 +1,5 @@
 import happ from "./hc-dna/workdir/perspective-diff-sync.happ";
 
-export const BUNDLE = Buffer.from(dna, "base64");
+export const BUNDLE = Buffer.from(happ, "base64");
 export const DNA_ROLE = "perspective-diff-sync";
 export const ZOME_NAME = "perspective_diff_sync";
