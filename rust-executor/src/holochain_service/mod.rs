25765ab39264fce0f8937161d710ca0e9aabd5ee
Nicolas Luck <nicolas@lucksus.eu>
2025-06-05 13:41:19 +0200

    Add ICE servers to Holochain's webRTC config

--- rust-executor/src/holochain_service/mod.rs
+++ rust-executor/src/holochain_service/mod.rs
@@ -379,6 +379,21 @@ impl HolochainService {
                 }));
             }
 
+            network_config.webrtc_config = Some(serde_json::json!({
+                "iceServers": [
+                    {"urls": ["stun:stun.cloudflare.com:3478"]},
+                    {"urls": ["stun:stun.l.gougle.com:19302"]},
+                    {
+                        "urls": [
+                            "stun:realy.ad4m.dev:3478",
+                            "turn:relay.ad4m.dev:443"
+                        ],
+                        "username": "openrelay",
+                        "credential": "openrelay",
+                    }
+                ]
+            }));
+
             config.network = network_config;
 
             config
