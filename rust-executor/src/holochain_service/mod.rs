08f738f29eeb71000f81eeb85e37d7fc69028ce4
Nicolas Luck <nicolas@lucksus.eu>
2026-01-15 13:35:51 +0100

    Use our bootstrap server

--- rust-executor/src/holochain_service/mod.rs
+++ rust-executor/src/holochain_service/mod.rs
@@ -427,13 +427,13 @@ impl HolochainService {
             //if local_config.use_bootstrap {
             //    network_config.bootstrap_url = Url2::parse(local_config.bootstrap_url.as_str());
             //} else {
-            //    network_config.bootstrap_url = Url2::parse("http://relay.ad4m.dev:4444");
+                network_config.bootstrap_url = Url2::parse("http://relay.ad4m.dev:4433");
             //}
 
             //if local_config.use_proxy {
             //    network_config.signal_url = Url2::parse(local_config.proxy_url.as_str());
             //} else {
-            //    network_config.signal_url = Url2::parse("ws://relay.ad4m.dev:4444");
+                network_config.signal_url = Url2::parse("ws://relay.ad4m.dev:4433");
             //}
 
             //if let Some(relay_url) = local_config.relay_url {
