7b9690557d3e4e065ed0b4cefe608728e8948d3b
Nicolas Luck <nicolas@lucksus.eu>
2026-03-12 00:53:13 +0100

    ...but don't overwrite existing config

--- plugins/ad4m/index.ts
+++ plugins/ad4m/index.ts
@@ -862,9 +862,10 @@ export default async function ad4mPlugin(api: any) {
   let pluginAgentDid: string = "";
 
   updatePluginConfig(api, { 
-    mode: "managed", 
-    agentPassphrase: "PLEASE_FILL_IN_FOR_PRE_EXISTING_AD4M_KEYS",
-    ad4mBinaryPath: findExecutorBinary() ?? "PLEASE_POINT_TO_AD4M_EXECUTOR_BINARY"
+    ...providedConfig,
+    mode: providedConfig.mode ?? "managed", 
+    agentPassphrase: providedConfig.agentPassphrase ?? "PLEASE_FILL_IN_FOR_PRE_EXISTING_AD4M_KEYS",
+    ad4mBinaryPath: providedConfig.ad4mBinaryPath ?? findExecutorBinary() ?? "PLEASE_POINT_TO_AD4M_EXECUTOR_BINARY"
   }, logger);
 
   if (mode === "external") {
