29ff68704520a00e79c0c0510a908008be71f917
Nicolas Luck <nicolas@lucksus.eu>
2026-03-12 17:52:42 +0100

    Switch to our new bootstrap/relay service

--- rust-executor/src/config.rs
+++ rust-executor/src/config.rs
@@ -126,10 +126,10 @@ impl Ad4mConfig {
             self.connect_holochain = Some(false);
         }
         if self.hc_proxy_url.is_none() {
-            self.hc_proxy_url = Some("ws://relay.ad4m.dev:4433".to_string());
+            self.hc_proxy_url = Some("ws://bootstrap.ad4m.dev:4433".to_string());
         }
         if self.hc_bootstrap_url.is_none() {
-            self.hc_bootstrap_url = Some("http://relay.ad4m.dev:4433".to_string());
+            self.hc_bootstrap_url = Some("http://boostrap.ad4m.dev:4433".to_string());
         }
         if self.hc_use_bootstrap.is_none() {
             self.hc_use_bootstrap = Some(true);
