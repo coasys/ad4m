fb64941b122dd2162a44972410f689b934acb4b7
HexaField <10372036+HexaField@users.noreply.github.com>
2026-05-07 18:05:04 +1000

    chore: remove benchmarks directory (re-added by merge from dev)

--- benchmarks/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { Channel } from './channel';
-export { Message } from './message';
-export { Conversation } from './conversation';
-export { ConversationSubgroup } from './conversation-subgroup';
-export * from './predicates';
