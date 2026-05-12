8245cc01384606507ebc5e9f0fbb2ac6bcf69c1a
Nicolas Luck <nicolas@lucksus.eu>
2026-05-12 20:45:38 +0200

    set version 0.13.0-test-6

--- rust-executor/src/globals.rs
+++ rust-executor/src/globals.rs
@@ -4,7 +4,7 @@ use tokio::sync::oneshot;
 
 lazy_static! {
     /// The current version of AD4M
-    pub static ref AD4M_VERSION: String = String::from("0.13.0-test-3");
+    pub static ref AD4M_VERSION: String = String::from("0.13.0-test-6");
 }
 
 /// Global shutdown signal sender. Used by `runtime_quit` GQL mutation and signal handlers
